9344ab7b53d3fccfe993c6d3568edf06b799e9ca
Trey West <trwest@redhat.com>
2023-06-14 16:53:52 -0400

    Add safe glogging to configmap pkg

--- pkg/configmap/configmap.go
+++ pkg/configmap/configmap.go
@@ -96,11 +96,11 @@ func NewBuilder(apiClient *clients.Settings, name, nsname string) *Builder {
 
 // Create makes a configmap in cluster and stores the created object in struct.
 func (builder *Builder) Create() (*Builder, error) {
-	glog.V(100).Infof("Creating the configmap %s in namespace %s", builder.Definition.Name, builder.Definition.Namespace)
-
-	if builder.errorMsg != "" {
-		return nil, fmt.Errorf(builder.errorMsg)
+	if valid, err := builder.validate(); !valid {
+		return builder, err
 	}
+
+	glog.V(100).Infof("Creating the configmap %s in namespace %s", builder.Definition.Name, builder.Definition.Namespace)
 
 	var err error
 	if !builder.Exists() {
@@ -113,6 +113,10 @@ func (builder *Builder) Create() (*Builder, error) {
 
 // Delete removes a configmap.
 func (builder *Builder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
 	glog.V(100).Infof("Deleting the configmap %s from namespace %s", builder.Definition.Name, builder.Definition.Namespace)
 
 	if !builder.Exists() {
@@ -133,6 +137,10 @@ func (builder *Builder) Delete() error {
 
 // Exists checks whether the given configmap exists.
 func (builder *Builder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
 	glog.V(100).Infof(
 		"Checking if configmap %s exists in namespace %s",
 		builder.Definition.Name, builder.Definition.Namespace)
@@ -146,23 +154,16 @@ func (builder *Builder) Exists() bool {
 
 // WithData defines the data placed in the configmap.
 func (builder *Builder) WithData(data map[string]string) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
 	glog.V(100).Infof(
 		"Creating configmap %s in namespace %s with this data: %s",
 		builder.Definition.Name, builder.Definition.Namespace, data)
 
-	if builder.errorMsg != "" {
-		return builder
-	}
-
 	if len(data) == 0 {
 		builder.errorMsg = "'data' cannot be empty"
-
-		return builder
-	}
-
-	// Make sure NewBuilder was already called to set builder.Definition.
-	if builder.Definition == nil {
-		builder.errorMsg = "can not redefine undefined configmap"
 
 		return builder
 	}
@@ -174,17 +175,11 @@ func (builder *Builder) WithData(data map[string]string) *Builder {
 
 // WithOptions creates configmap with generic mutation options.
 func (builder *Builder) WithOptions(options ...AdditionalOptions) *Builder {
-	glog.V(100).Infof("Setting configmap additional options")
-
-	if builder.Definition == nil {
-		glog.V(100).Infof("The configmap is undefined")
-
-		builder.errorMsg = msg.UndefinedCrdObjectErrString("configmap")
-	}
-
-	if builder.errorMsg != "" {
+	if valid, _ := builder.validate(); !valid {
 		return builder
 	}
+
+	glog.V(100).Infof("Setting configmap additional options")
 
 	for _, option := range options {
 		if option != nil {
@@ -208,4 +203,28 @@ func GetGVR() schema.GroupVersionResource {
 	return schema.GroupVersionResource{
 		Group: "", Version: "v1", Resource: "configmaps",
 	}
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *Builder) validate() (bool, error) {
+	if builder == nil {
+		glog.V(100).Infof("The builder is uninitialized")
+
+		return false, fmt.Errorf("error: received nil builder")
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The configmap is undefined")
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString("ConfigMap")
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The builder has error message: %s", builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
 }
