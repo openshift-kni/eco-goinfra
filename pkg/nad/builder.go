a927df0134305a526ff762c2882292088446e33e
Trey West <trwest@redhat.com>
2023-06-15 14:24:53 -0400

    Add safe glogging to nad pkg

--- pkg/nad/builder.go
+++ pkg/nad/builder.go
@@ -104,12 +104,12 @@ func Pull(apiClient *clients.Settings, name, nsname string) (*Builder, error) {
 //
 //	an error if any occurred.
 func (builder *Builder) Create() (*Builder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
 	glog.V(100).Infof("Creating NetworkAttachmentDefinition %s in namespace %s",
 		builder.Definition.Name, builder.Definition.Namespace)
-
-	if builder.errorMsg != "" {
-		return nil, fmt.Errorf(builder.errorMsg)
-	}
 
 	err := builder.fillConfigureString()
 
@@ -132,6 +132,10 @@ func (builder *Builder) Create() (*Builder, error) {
 // (If NAD doesn't exist, nothing is done) and a nil error is returned.
 // return value:    an error if any occurred.
 func (builder *Builder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
 	glog.V(100).Infof("Deleting NetworkAttachmentDefinition %s in namespace %s",
 		builder.Definition.Name, builder.Definition.Namespace)
 
@@ -156,6 +160,10 @@ func (builder *Builder) Delete() error {
 //
 //	false   - NAD doesn't exist.
 func (builder *Builder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
 	glog.V(100).Infof("Checking if NetworkAttachmentDefinition %s exists in namespace %s",
 		builder.Definition.Name, builder.Definition.Namespace)
 
@@ -168,6 +176,10 @@ func (builder *Builder) Exists() bool {
 // GetString prints NetworkAttachmentDefinition resource.
 // return value:    the builder details in json string format, and an error if any occurred.
 func (builder *Builder) GetString() (string, error) {
+	if valid, err := builder.validate(); !valid {
+		return "", err
+	}
+
 	glog.V(100).Infof("Returning NetworkAttachmentDefinition resource in json format")
 
 	nadByte, err := json.MarshalIndent(builder.Definition, "", "    ")
@@ -181,6 +193,10 @@ func (builder *Builder) GetString() (string, error) {
 // fillConfigureString adds a configuration string to builder definition specs configuration if needed.
 // return value:    an error if any occurred.
 func (builder *Builder) fillConfigureString() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
 	glog.V(100).Infof("Adding configuration to NetworkAttachmentDefinition builder if needed")
 
 	if builder.metaPluginConfigs == nil {
@@ -209,11 +225,11 @@ func (builder *Builder) fillConfigureString() error {
 
 // WithMasterPlugin defines master plugin configuration in the NetworkAttachmentDefinition spec.
 func (builder *Builder) WithMasterPlugin(masterPlugin *MasterPlugin) *Builder {
-	glog.V(100).Infof("Adding masterPlugin %v to NAD %s", masterPlugin, builder.Definition.Name)
-
-	if builder.Definition == nil {
-		builder.errorMsg = msg.UndefinedCrdObjectErrString("NAD")
+	if valid, _ := builder.validate(); !valid {
+		return builder
 	}
+
+	glog.V(100).Infof("Adding masterPlugin %v to NAD %s", masterPlugin, builder.Definition.Name)
 
 	emptyNadConfig := nadV1.NetworkAttachmentDefinitionSpec{}
 
@@ -237,4 +253,36 @@ func GetGVR() schema.GroupVersionResource {
 	return schema.GroupVersionResource{
 		Group: "k8s.cni.cncf.io", Version: "v1", Resource: "network-attachment-definitions",
 	}
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *Builder) validate() (bool, error) {
+	resourceCRD := "NetworkAttachmentDefinition"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString(resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
 }
