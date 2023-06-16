e625b7a6775d5c46107de93472e0f791ebf63b47
Trey West <trwest@redhat.com>
2023-06-16 09:02:29 -0400

    Add safe glogging to network pkg

--- pkg/network/operator.go
+++ pkg/network/operator.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
 	operatorV1 "github.com/openshift/api/operator/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -49,6 +50,10 @@ func PullOperator(apiClient *clients.Settings) (*OperatorBuilder, error) {
 
 // Exists checks whether the given network.operator exists.
 func (builder *OperatorBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
 	glog.V(100).Infof(
 		"Checking if network.operator %s exists",
 		builder.Definition.Name)
@@ -61,6 +66,10 @@ func (builder *OperatorBuilder) Exists() bool {
 
 // Get returns network.operator object.
 func (builder *OperatorBuilder) Get() (*operatorV1.Network, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
 	clusterNetwork := &operatorV1.Network{}
 	err := builder.apiClient.Get(context.TODO(), goclient.ObjectKey{
 		Name: builder.Definition.Name,
@@ -75,13 +84,13 @@ func (builder *OperatorBuilder) Get() (*operatorV1.Network, error) {
 
 // Update renovates the existing network.operator object with the new definition in builder.
 func (builder *OperatorBuilder) Update() (*OperatorBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
 	glog.V(100).Infof("Updating the network.operator object %s",
 		builder.Definition.Name,
 	)
-
-	if builder.errorMsg != "" {
-		return nil, fmt.Errorf(builder.errorMsg)
-	}
 
 	err := builder.apiClient.Update(context.TODO(), builder.Definition)
 
@@ -90,6 +99,10 @@ func (builder *OperatorBuilder) Update() (*OperatorBuilder, error) {
 
 // SetLocalGWMode switches network.operator OVN mode from/to local mode.
 func (builder *OperatorBuilder) SetLocalGWMode(state bool, timeout time.Duration) (*OperatorBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
 	var err error
 
 	if builder.Definition.Spec.DefaultNetwork.OVNKubernetesConfig.GatewayConfig.RoutingViaHost != state {
@@ -125,6 +138,10 @@ func (builder *OperatorBuilder) SetLocalGWMode(state bool, timeout time.Duration
 // specified condition type with the expected status.
 func (builder *OperatorBuilder) WaitUntilInCondition(
 	condition string, timeout time.Duration, status operatorV1.ConditionStatus) error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
 	glog.V(100).Infof("Wait until network.operator object %s is in condition %v",
 		builder.Definition.Name, condition)
 
@@ -144,4 +161,36 @@ func (builder *OperatorBuilder) WaitUntilInCondition(
 	})
 
 	return err
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *OperatorBuilder) validate() (bool, error) {
+	resourceCRD := "Network.Operator"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString(resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
 }
