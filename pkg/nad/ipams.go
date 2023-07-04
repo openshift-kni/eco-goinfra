356f93cbcbb5e7eb2c4349abfbfe56485f24c7bf
nkononov <nkononov@redhat.com>
2023-07-03 21:50:23 -0400

    nad pkg added IpVlanPlugin and whereabout IPAM

--- pkg/nad/ipams.go
+++ pkg/nad/ipams.go
@@ -4,3 +4,33 @@ package nad
 func IPAMStatic() *IPAM {
 	return &IPAM{Type: "static"}
 }
+
+// IPAMWhereAbout returns WhereAbout ipam type.
+func IPAMWhereAbout(ipRange, gateway string) *IPAM {
+	ipam := &IPAM{Type: "whereabouts", IPRanges: []IPRanges{{Range: ipRange, Gateway: gateway}}}
+
+	if ipRange == "" {
+		return nil
+	}
+
+	if gateway == "" {
+		return nil
+	}
+
+	return ipam
+}
+
+// WhereAboutAppendRange returns WhereAbout ipam type with additional address range.
+func WhereAboutAppendRange(ipam *IPAM, ipRange, gateway string) *IPAM {
+	ipam.IPRanges = append(ipam.IPRanges, IPRanges{Range: ipRange, Gateway: gateway})
+
+	if ipRange == "" {
+		return nil
+	}
+
+	if gateway == "" {
+		return nil
+	}
+
+	return ipam
+}
