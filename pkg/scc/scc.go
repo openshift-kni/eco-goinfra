aa7c80c5edaabf9b89895b87427889906990c6ed
Nikita <meerovicc@gmail.com>
2023-07-24 21:10:17 -0400

    Added SecurityContextConstraints package (#72)
    * Added SecurityContextConstraints package
    
    * Fixed few typos
    
    * validate method moved up before glog. Fixed allowPrivilegedEscalation
    method
    
    ---------
    
    Co-authored-by: nkononov <nkononov@redhat.com>

---
+++ pkg/scc/scc.go
@@ -0,0 +1,384 @@
+package scc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
+	securityV1 "github.com/openshift/api/security/v1"
+	coreV1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Builder provides struct for SecurityContextConstraints object containing connection
+// to the cluster SecurityContextConstraints definition.
+type Builder struct {
+	// SecurityContextConstraints definition. Used to create SecurityContextConstraints object
+	Definition *securityV1.SecurityContextConstraints
+	// Created SecurityContextConstraints object
+	Object *securityV1.SecurityContextConstraints
+	// Used in functions that define or mutate SecurityContextConstraints definition. errorMsg is processed
+	// before the SecurityContextConstraints object is created
+	errorMsg  string
+	apiClient *clients.Settings
+}
+
+// SecurityContextConstraintsAdditionalOptions additional options for SecurityContextConstraints object.
+type SecurityContextConstraintsAdditionalOptions func(builder *Builder) (*Builder, error)
+
+// NewBuilder creates new instance of Builder.
+func NewBuilder(apiClient *clients.Settings, name, runAsUser, selinuxContext string) *Builder {
+	glog.V(100).Infof(
+		"Initializing new SecurityContextConstraints structure with the following params: "+
+			"name: %s, runAsUser type: %s, selinuxContext type: %s", name, runAsUser, selinuxContext)
+
+	builder := Builder{
+		apiClient: apiClient,
+		Definition: &securityV1.SecurityContextConstraints{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name: name,
+			},
+			RunAsUser: securityV1.RunAsUserStrategyOptions{
+				Type: securityV1.RunAsUserStrategyType(runAsUser),
+			},
+			SELinuxContext: securityV1.SELinuxContextStrategyOptions{
+				Type: securityV1.SELinuxContextStrategyType(selinuxContext),
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the SecurityContextConstraints is empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'name' cannot be empty"
+	}
+
+	if runAsUser == "" {
+		glog.V(100).Infof("The runAsUser of the SecurityContextConstraints is empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'runAsUser' cannot be empty"
+	}
+
+	if selinuxContext == "" {
+		glog.V(100).Infof("The selinuxContext of the SecurityContextConstraints is empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'selinuxContext' cannot be empty"
+	}
+
+	return &builder
+}
+
+// Pull pulls existing SecurityContextConstraints from cluster.
+func Pull(apiClient *clients.Settings, name string) (*Builder, error) {
+	glog.V(100).Infof("Pulling existing SecurityContextConstraints object name %s from cluster", name)
+
+	builder := Builder{
+		apiClient: apiClient,
+		Definition: &securityV1.SecurityContextConstraints{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name: name,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the SecurityContextConstraints is empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'name' cannot be empty"
+	}
+
+	if !builder.Exists() {
+		return nil, fmt.Errorf("SecurityContextConstraints object %s doesn't exist", name)
+	}
+
+	builder.Definition = builder.Object
+
+	return &builder, nil
+}
+
+// WithPrivilegedContainer adds bool flag to the allowPrivilegedContainer of SecurityContextConstraints.
+func (builder *Builder) WithPrivilegedContainer(allowPrivileged bool) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		" AllowPrivilegedContainer: %t flag", builder.Definition.Name, allowPrivileged)
+
+	builder.Definition.AllowPrivilegedContainer = allowPrivileged
+
+	return builder
+}
+
+// WithPrivilegedEscalation adds bool flag to the allowPrivilegeEscalation of SecurityContextConstraints.
+func (builder *Builder) WithPrivilegedEscalation(allowPrivilegedEscalation bool) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		" allowPrivilegedEscalation: %t flag", builder.Definition.Name, allowPrivilegedEscalation)
+
+	builder.Definition.DefaultAllowPrivilegeEscalation = &allowPrivilegedEscalation
+
+	return builder
+}
+
+// WithDropCapabilities adds list of drop capabilities to SecurityContextConstraints.
+func (builder *Builder) WithDropCapabilities(requiredDropCapabilities []coreV1.Capability) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"requiredDropCapabilities: %v", builder.Definition.Name, requiredDropCapabilities)
+
+	if len(requiredDropCapabilities) == 0 {
+		glog.V(100).Infof("SecurityContextConstraints 'requiredDropCapabilities' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'requiredDropCapabilities' cannot be empty list"
+
+		return builder
+	}
+
+	if builder.Definition.RequiredDropCapabilities == nil {
+		builder.Definition.RequiredDropCapabilities = requiredDropCapabilities
+
+		return builder
+	}
+
+	builder.Definition.RequiredDropCapabilities = append(
+		builder.Definition.RequiredDropCapabilities, requiredDropCapabilities...)
+
+	return builder
+}
+
+// WithAllowCapabilities adds list of allow capabilities to SecurityContextConstraints.
+func (builder *Builder) WithAllowCapabilities(allowCapabilities []coreV1.Capability) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"allowCapabilities: %v", builder.Definition.Name, allowCapabilities)
+
+	if len(allowCapabilities) == 0 {
+		glog.V(100).Infof("SecurityContextConstraints 'allowCapabilities' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'allowCapabilities' cannot be empty list"
+
+		return builder
+	}
+
+	if builder.Definition.AllowedCapabilities == nil {
+		builder.Definition.AllowedCapabilities = allowCapabilities
+
+		return builder
+	}
+
+	builder.Definition.AllowedCapabilities = append(builder.Definition.AllowedCapabilities, allowCapabilities...)
+
+	return builder
+}
+
+// WithFSGroup adds fsGroup to SecurityContextConstraints.
+func (builder *Builder) WithFSGroup(fsGroup string) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"fsGroup: %s", builder.Definition.Name, fsGroup)
+
+	if fsGroup == "" {
+		glog.V(100).Infof("SecurityContextConstraints 'fsGroup' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'fsGroup' cannot be empty string"
+
+		return builder
+	}
+
+	builder.Definition.FSGroup.Type = securityV1.FSGroupStrategyType(fsGroup)
+
+	return builder
+}
+
+// WithSeccompProfiles adds list of seccompProfiles to SecurityContextConstraints.
+func (builder *Builder) WithSeccompProfiles(seccompProfiles []string) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"SeccompProfiles: %v", builder.Definition.Name, seccompProfiles)
+
+	if len(seccompProfiles) == 0 {
+		glog.V(100).Infof("SecurityContextConstraints 'seccompProfiles' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'seccompProfiles' cannot be empty list"
+
+		return builder
+	}
+
+	if builder.Definition.SeccompProfiles == nil {
+		builder.Definition.SeccompProfiles = seccompProfiles
+
+		return builder
+	}
+
+	builder.Definition.SeccompProfiles = append(builder.Definition.SeccompProfiles, seccompProfiles...)
+
+	return builder
+}
+
+// WithSupplementalGroups adds SupplementalGroups to SecurityContextConstraints.
+func (builder *Builder) WithSupplementalGroups(supplementalGroupsType string) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"supplementalGroupsType: %s", builder.Definition.Name, supplementalGroupsType)
+
+	if supplementalGroupsType == "" {
+		glog.V(100).Infof("SecurityContextConstraints 'SupplementalGroups' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'SupplementalGroups' cannot be empty string"
+
+		return builder
+	}
+
+	builder.Definition.SupplementalGroups.Type = securityV1.SupplementalGroupsStrategyType(supplementalGroupsType)
+
+	return builder
+}
+
+// WithUsers adds users to SecurityContextConstraints.
+func (builder *Builder) WithUsers(users []string) *Builder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof("Redefining SecurityContextConstraints %s with"+
+		"users: %v", builder.Definition.Name, users)
+
+	if len(users) == 0 {
+		glog.V(100).Infof("SecurityContextConstraints 'users' argument cannot be empty")
+
+		builder.errorMsg = "SecurityContextConstraints 'users' cannot be empty list"
+
+		return builder
+	}
+
+	if builder.Definition.Users == nil {
+		builder.Definition.Users = users
+
+		return builder
+	}
+
+	builder.Definition.Users = append(builder.Definition.Users, users...)
+
+	return builder
+}
+
+// Create generates a SecurityContextConstraints and stores the created object in struct.
+func (builder *Builder) Create() (*Builder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Creating SecurityContextConstraints %s", builder.Definition.Name)
+
+	var err error
+	if !builder.Exists() {
+		builder.Object, err = builder.apiClient.SecurityContextConstraints().Create(
+			context.TODO(), builder.Definition, metaV1.CreateOptions{})
+	}
+
+	return builder, err
+}
+
+// Delete removes a SecurityContextConstraints.
+func (builder *Builder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
+	glog.V(100).Infof("Removing SecurityContextConstraints %s", builder.Definition.Name)
+
+	if !builder.Exists() {
+		return nil
+	}
+
+	err := builder.apiClient.SecurityContextConstraints().Delete(
+		context.TODO(), builder.Object.Name, metaV1.DeleteOptions{})
+
+	builder.Object = nil
+
+	return err
+}
+
+// Update modifies an existing SecurityContextConstraints in the cluster.
+func (builder *Builder) Update() (*Builder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Updating SecurityContextConstraints %s ", builder.Definition.Name)
+
+	var err error
+	builder.Object, err = builder.apiClient.SecurityContextConstraints().Update(
+		context.TODO(), builder.Definition, metaV1.UpdateOptions{})
+
+	return builder, err
+}
+
+// Exists checks whether the given SecurityContextConstraints exists.
+func (builder *Builder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if SecurityContextConstraints %s exists", builder.Definition.Name)
+
+	var err error
+	builder.Object, err = builder.apiClient.SecurityContextConstraints().Get(
+		context.Background(), builder.Definition.Name, metaV1.GetOptions{})
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *Builder) validate() (bool, error) {
+	resourceCRD := "SecurityContextConstraints"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString(resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
