f9e8023e62d71821825efde210c49ea573d60f41
Constantin Daniel Vultur <54892201+cdvultur@users.noreply.github.com>
2023-08-21 14:53:12 +0200

    KMM: Fix panic while calling WithModprobeSpec (#89)

--- pkg/kmm/containers.go
+++ pkg/kmm/containers.go
@@ -59,7 +59,17 @@ func (builder *ModuleLoaderContainerBuilder) WithModprobeSpec(
 	builder.definition.Modprobe.DirName = dirName
 	builder.definition.Modprobe.FirmwarePath = fwPath
 	builder.definition.Modprobe.Parameters = parameters
+
+	if builder.definition.Modprobe.Args == nil {
+		builder.definition.Modprobe.Args = &moduleV1Beta1.ModprobeArgs{}
+	}
+
 	builder.definition.Modprobe.Args.Load = args
+
+	if builder.definition.Modprobe.RawArgs == nil {
+		builder.definition.Modprobe.RawArgs = &moduleV1Beta1.ModprobeArgs{}
+	}
+
 	builder.definition.Modprobe.RawArgs.Load = rawargs
 
 	return builder
