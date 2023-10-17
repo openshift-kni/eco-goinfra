c14cb696a01b25cf13ad5ece103afb51eb6018c7
Elena German <elgerman@redhat.com>
2023-10-17 08:34:45 -0400

    servicemesh pkg added

---
+++ pkg/servicemesh/control-plane.go
@@ -0,0 +1,505 @@
+package servicemesh
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	istioV2 "maistra.io/api/core/v2"
+	goclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ControlPlaneBuilder provides a struct for serviceMeshControlPlane object from the cluster and
+// a serviceMeshControlPlane definition.
+type ControlPlaneBuilder struct {
+	// serviceMeshControlPlane definition, used to create the serviceMeshControlPlane object.
+	Definition *istioV2.ServiceMeshControlPlane
+	// Created serviceMeshControlPlane object.
+	Object *istioV2.ServiceMeshControlPlane
+	// Used in functions that define or mutate serviceMeshControlPlane definition. errorMsg is processed
+	// before the serviceMeshControlPlane object is created.
+	errorMsg string
+	// api client to interact with the cluster.
+	apiClient *clients.Settings
+}
+
+// NewControlPlaneBuilder method creates new instance of builder.
+func NewControlPlaneBuilder(apiClient *clients.Settings, name, nsname string) *ControlPlaneBuilder {
+	glog.V(100).Infof("Initializing new ControlPlaneBuilder structure with the following "+
+		"params: name: %s, namespace: %s", name, nsname)
+
+	builder := &ControlPlaneBuilder{
+		apiClient: apiClient,
+		Definition: &istioV2.ServiceMeshControlPlane{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the serviceMeshControlPlane is empty")
+
+		builder.errorMsg = "the serviceMeshControlPlane 'name' cannot be empty"
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The namespace of the serviceMeshControlPlane is empty")
+
+		builder.errorMsg = "the serviceMeshControlPlane 'namespace' cannot be empty"
+	}
+
+	return builder
+}
+
+// WithAllAddonsDisabled disables all addons to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithAllAddonsDisabled() *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	enablement := false
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with the all addons disabled",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	disableAllAddons := istioV2.AddonsConfig{
+		Prometheus: &istioV2.PrometheusAddonConfig{
+			Enablement: istioV2.Enablement{Enabled: &enablement},
+		},
+		Grafana: &istioV2.GrafanaAddonConfig{
+			Enablement: istioV2.Enablement{Enabled: &enablement},
+		},
+		Kiali: &istioV2.KialiAddonConfig{
+			Enablement: istioV2.Enablement{Enabled: &enablement},
+		},
+		ThreeScale: &istioV2.ThreeScaleAddonConfig{
+			Enablement: istioV2.Enablement{Enabled: &enablement},
+		},
+	}
+
+	builder.Definition.Spec.Addons = &disableAllAddons
+
+	return builder
+}
+
+// WithGrafanaAddon adds grafana addon to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithGrafanaAddon(
+	enablement bool,
+	grafanaInstallConfig *istioV2.GrafanaInstallConfig,
+	address string) *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with the Grafana addons defined: enablement %s, "+
+			"grafanaInstallConfig %v, address %s", builder.Definition.Name, builder.Definition.Namespace,
+		enablement, grafanaInstallConfig, address)
+
+	if enablement {
+		if grafanaInstallConfig == nil {
+			glog.V(100).Infof("The grafanaInstallConfig of the Grafana addon is empty")
+
+			builder.errorMsg = "the Grafana addon 'grafanaInstallConfig' cannot be empty when Grafana addon is enabled"
+		}
+
+		if address == "" {
+			glog.V(100).Infof("The address of the Grafana addon is empty")
+
+			builder.errorMsg = "the Grafana addon 'address' cannot be empty when Grafana addon is enabled"
+		}
+	}
+
+	if builder.errorMsg != "" {
+		return builder
+	}
+
+	addonConfig := &istioV2.GrafanaAddonConfig{
+		Enablement: istioV2.Enablement{
+			Enabled: &enablement,
+		},
+		Install: grafanaInstallConfig,
+		Address: &address,
+	}
+
+	if builder.Definition.Spec.Addons == nil {
+		builder.Definition.Spec.Addons = new(istioV2.AddonsConfig)
+	}
+
+	builder.Definition.Spec.Addons.Grafana = addonConfig
+
+	return builder
+}
+
+// WithJaegerAddon adds jaeger addon to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithJaegerAddon(
+	name string,
+	jaegerInstallConfig *istioV2.JaegerInstallConfig) *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with the JaegerAddonConfig defined",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if name == "" {
+		glog.V(100).Infof("The name of the Jaeger addon is empty")
+
+		builder.errorMsg = "the Jaeger addon 'name' cannot be empty"
+	}
+
+	if jaegerInstallConfig == nil {
+		glog.V(100).Infof("The jaegerInstallConfig of the Jaeger addon is empty")
+
+		builder.errorMsg = "the Jaeger addon 'jaegerInstallConfig' cannot be empty"
+	}
+
+	if builder.errorMsg != "" {
+		return builder
+	}
+
+	addonConfig := &istioV2.JaegerAddonConfig{
+		Name:    name,
+		Install: jaegerInstallConfig,
+	}
+
+	if builder.Definition.Spec.Addons == nil {
+		builder.Definition.Spec.Addons = new(istioV2.AddonsConfig)
+	}
+
+	builder.Definition.Spec.Addons.Jaeger = addonConfig
+
+	return builder
+}
+
+// WithKialiAddon adds kiali addons to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithKialiAddon(
+	enablement bool,
+	name string,
+	kialiInstallConfig *istioV2.KialiInstallConfig) *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with the KialiAddonConfig defined",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if enablement {
+		if kialiInstallConfig == nil {
+			glog.V(100).Infof("The kialiInstallConfig of the Kiali addon is empty")
+
+			builder.errorMsg = "the Kiali addon 'kialiInstallConfig' cannot be empty when Kiali addon is enabled"
+		}
+
+		if name == "" {
+			glog.V(100).Infof("The name of the Kiali addon is empty")
+
+			builder.errorMsg = "the Kiali addon 'name' cannot be empty when Kiali addon is enabled"
+		}
+	}
+
+	if builder.errorMsg != "" {
+		return builder
+	}
+
+	addonConfig := &istioV2.KialiAddonConfig{
+		Enablement: istioV2.Enablement{
+			Enabled: &enablement,
+		},
+		Name:    name,
+		Install: kialiInstallConfig,
+	}
+
+	if builder.Definition.Spec.Addons == nil {
+		builder.Definition.Spec.Addons = new(istioV2.AddonsConfig)
+	}
+
+	builder.Definition.Spec.Addons.Kiali = addonConfig
+
+	return builder
+}
+
+// WithPrometheusAddon adds prometheus addons to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithPrometheusAddon(
+	enablement bool,
+	scrape bool,
+	metricsExpiryDuration string,
+	address string,
+	prometheusInstallConfig *istioV2.PrometheusInstallConfig) *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with the PrometheusAddonConfig defined",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if enablement {
+		if prometheusInstallConfig == nil {
+			glog.V(100).Info("The prometheusInstallConfig of the Prometheus addon is empty")
+
+			builder.errorMsg = "the Prometheus addon 'prometheusInstallConfig' cannot " +
+				"be empty when Prometheus addon is enabled"
+		}
+
+		if metricsExpiryDuration == "" {
+			glog.V(100).Info("The metricsExpiryDuration of the Prometheus addon is empty")
+
+			builder.errorMsg = "the Prometheus addon 'metricsExpiryDuration' cannot " +
+				"be empty when Prometheus addon is enabled"
+		}
+
+		if address == "" {
+			glog.V(100).Info("The address of the Prometheus addon is empty")
+
+			builder.errorMsg = "the Prometheus addon 'address' cannot be empty when Prometheus addon is enabled"
+		}
+	}
+
+	if builder.errorMsg != "" {
+		return builder
+	}
+
+	addonConfig := &istioV2.PrometheusAddonConfig{
+		Enablement: istioV2.Enablement{
+			Enabled: &enablement,
+		},
+		MetricsExpiryDuration: metricsExpiryDuration,
+		Scrape:                &scrape,
+		Install:               prometheusInstallConfig,
+		Address:               &address,
+	}
+
+	if builder.Definition.Spec.Addons == nil {
+		builder.Definition.Spec.Addons = new(istioV2.AddonsConfig)
+	}
+
+	builder.Definition.Spec.Addons.Prometheus = addonConfig
+
+	return builder
+}
+
+// WithGatewaysEnablement adds gateway enablement to the serviceMeshControlPlane.
+func (builder *ControlPlaneBuilder) WithGatewaysEnablement(enablement bool) *ControlPlaneBuilder {
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	glog.V(100).Infof(
+		"Creating serviceMeshControlPlane %s in namespace %s with enabled Gateways",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	gatewaysConfig := istioV2.Enablement{Enabled: &enablement}
+
+	if builder.Definition.Spec.Gateways == nil {
+		builder.Definition.Spec.Gateways = &istioV2.GatewaysConfig{}
+	}
+
+	builder.Definition.Spec.Gateways.Enablement = gatewaysConfig
+
+	return builder
+}
+
+// PullControlPlane retrieves an existing serviceMeshControlPlane object from the cluster.
+func PullControlPlane(apiClient *clients.Settings, name, nsname string) (*ControlPlaneBuilder, error) {
+	glog.V(100).Infof(
+		"Pulling serviceMeshControlPlane object name %s in namespace: %s", name, nsname)
+
+	builder := ControlPlaneBuilder{
+		apiClient: apiClient,
+		Definition: &istioV2.ServiceMeshControlPlane{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the serviceMeshControlPlane is empty")
+
+		builder.errorMsg = "serviceMeshControlPlane 'name' cannot be empty"
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The namespace of the serviceMeshControlPlane is empty")
+
+		builder.errorMsg = "serviceMeshControlPlane 'nsname' cannot be empty"
+	}
+
+	if builder.errorMsg != "" {
+		return &builder, nil
+	}
+
+	if !builder.Exists() {
+		return nil, fmt.Errorf("serviceMeshControlPlane object %s doesn't exist in namespace %s", name, nsname)
+	}
+
+	builder.Definition = builder.Object
+
+	return &builder, nil
+}
+
+// Get fetches existing serviceMeshControlPlane from cluster.
+func (builder *ControlPlaneBuilder) Get() (*istioV2.ServiceMeshControlPlane, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Pulling existing serviceMeshControlPlane with name %s in namespace %s from cluster",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	servicemeshcontrolplane := &istioV2.ServiceMeshControlPlane{}
+	err := builder.apiClient.Get(context.TODO(), goclient.ObjectKey{
+		Name:      builder.Definition.Name,
+		Namespace: builder.Definition.Namespace,
+	}, servicemeshcontrolplane)
+
+	if err != nil {
+		return nil, err
+	}
+
+	builder.Object = servicemeshcontrolplane
+
+	return servicemeshcontrolplane, nil
+}
+
+// Create makes a serviceMeshControlPlane in the cluster and stores the created object in struct.
+func (builder *ControlPlaneBuilder) Create() (*ControlPlaneBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Creating the serviceMeshControlPlane %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	if !builder.Exists() {
+		err = builder.apiClient.Create(context.TODO(), builder.Definition)
+		if err == nil {
+			builder.Object = builder.Definition
+		}
+	}
+
+	return builder, err
+}
+
+// Delete removes serviceMeshControlPlane from a cluster.
+func (builder *ControlPlaneBuilder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
+	glog.V(100).Infof("Deleting the serviceMeshControlPlane %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if !builder.Exists() {
+		return fmt.Errorf("serviceMeshControlPlane %s in namespace %s cannot be deleted because it does not exist",
+			builder.Definition.Name, builder.Definition.Namespace)
+	}
+
+	err := builder.apiClient.Delete(context.Background(), builder.Definition)
+
+	if err != nil {
+		return fmt.Errorf("can not delete serviceMeshControlPlane %s in namespace %s due to %w",
+			builder.Definition.Name, builder.Definition.Namespace, err)
+	}
+
+	builder.Object = nil
+
+	return nil
+}
+
+// Update renovates the existing serviceMeshControlPlane object with serviceMeshControlPlane definition in builder.
+func (builder *ControlPlaneBuilder) Update(force bool) (*ControlPlaneBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Info("Updating serviceMeshControlPlane %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	err := builder.apiClient.Update(context.TODO(), builder.Definition)
+
+	if err != nil {
+		if force {
+			glog.V(100).Infof("Failed to update the serviceMeshControlPlane object %s in namespace %s. "+
+				"Note: Force flag set, executed delete/create methods instead",
+				builder.Definition.Name, builder.Definition.Namespace)
+
+			err := builder.Delete()
+
+			if err != nil {
+				glog.V(100).Infof(
+					"Failed to update the serviceMeshControlPlane object %s in namespace %s."+
+						"due to error in delete function", builder.Definition.Name, builder.Definition.Namespace)
+
+				return nil, err
+			}
+
+			return builder.Create()
+		}
+	}
+
+	if err == nil {
+		builder.Object = builder.Definition
+	}
+
+	return builder, err
+}
+
+// Exists checks whether the given serviceMeshControlPlane exists.
+func (builder *ControlPlaneBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if serviceMeshControlPlane %s exists in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	builder.Object, err = builder.Get()
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *ControlPlaneBuilder) validate() (bool, error) {
+	resourceCRD := "ServiceMeshControlPlane"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString(resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The builder %s has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
---
+++ pkg/servicemesh/member-roll.go
@@ -0,0 +1,271 @@
+package servicemesh
+
+import (
+	"context"
+	"fmt"
+
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	istioV1 "maistra.io/api/core/v1"
+	goclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// MemberRollBuilder provides a struct for serviceMeshMemberRoll object from the cluster and
+// a serviceMeshMemberRoll definition.
+type MemberRollBuilder struct {
+	// serviceMeshMemberRoll definition, used to create the serviceMeshMemberRoll object.
+	Definition *istioV1.ServiceMeshMemberRoll
+	// Created serviceMeshMemberRoll object.
+	Object *istioV1.ServiceMeshMemberRoll
+	// Used in functions that define or mutate serviceMeshMemberRoll definition. errorMsg is processed
+	// before the serviceMeshMemberRoll object is created.
+	errorMsg string
+	// api client to interact with the cluster.
+	apiClient *clients.Settings
+}
+
+// NewMemberRollBuilder method creates new instance of builder.
+func NewMemberRollBuilder(apiClient *clients.Settings, name, nsname string) *MemberRollBuilder {
+	glog.V(100).Infof("Initializing new serviceMeshMemberRollBuilder structure with the following "+
+		"params: name: %s, namespace: %s", name, nsname)
+
+	builder := &MemberRollBuilder{
+		apiClient: apiClient,
+		Definition: &istioV1.ServiceMeshMemberRoll{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the serviceMeshMemberRoll is empty")
+
+		builder.errorMsg = "The serviceMeshMemberRoll 'name' cannot be empty"
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The namespace of the serviceMeshMemberRoll is empty")
+
+		builder.errorMsg = "The serviceMeshMemberRoll 'namespace' cannot be empty"
+	}
+
+	return builder
+}
+
+// PullMemberRole retrieves an existing serviceMeshMemberRoll object from the cluster.
+func PullMemberRole(apiClient *clients.Settings, name, nsname string) (*MemberRollBuilder, error) {
+	glog.V(100).Infof(
+		"Pulling serviceMeshMemberRoll object name: %s in namespace: %s", name, nsname)
+
+	builder := MemberRollBuilder{
+		apiClient: apiClient,
+		Definition: &istioV1.ServiceMeshMemberRoll{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the serviceMeshMemberRoll is empty")
+
+		builder.errorMsg = "serviceMeshMemberRoll 'name' cannot be empty"
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The namespace of the serviceMeshMemberRoll is empty")
+
+		builder.errorMsg = "serviceMeshMemberRoll 'nsname' cannot be empty"
+	}
+
+	if !builder.Exists() {
+		return nil, fmt.Errorf("serviceMeshMemberRoll oject %s doesn't exist in namespace %s", name, nsname)
+	}
+
+	builder.Definition = builder.Object
+
+	return &builder, nil
+}
+
+// Get fetches existing serviceMeshMemberRoll from cluster.
+func (builder *MemberRollBuilder) Get() (*istioV1.ServiceMeshMemberRoll, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Fetching existing serviceMeshMemberRoll with name %s under namespace %s from cluster",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	servicemeshmemberroll := &istioV1.ServiceMeshMemberRoll{}
+	err := builder.apiClient.Get(context.TODO(), goclient.ObjectKey{
+		Name:      builder.Definition.Name,
+		Namespace: builder.Definition.Namespace,
+	}, servicemeshmemberroll)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return servicemeshmemberroll, nil
+}
+
+// Create makes a serviceMeshMemberRoll in the cluster and stores the created object in struct.
+func (builder *MemberRollBuilder) Create() (*MemberRollBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Creating the serviceMeshMemberRoll %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	if !builder.Exists() {
+		err = builder.apiClient.Create(context.TODO(), builder.Definition)
+		if err == nil {
+			builder.Object = builder.Definition
+		}
+	}
+
+	return builder, err
+}
+
+// Delete removes serviceMeshMemberRoll from a cluster.
+func (builder *MemberRollBuilder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
+	glog.V(100).Infof("Deleting the serviceMeshMemberRoll %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if !builder.Exists() {
+		return fmt.Errorf("serviceMeshMemberRoll %s in namespace %s cannot be deleted because it does not exist",
+			builder.Definition.Name, builder.Definition.Namespace)
+	}
+
+	err := builder.apiClient.Delete(context.Background(), builder.Definition)
+
+	if err != nil {
+		return fmt.Errorf("can not delete serviceMeshMemberRoll %s in namespace %s due to %w",
+			builder.Definition.Name, builder.Definition.Namespace, err)
+	}
+
+	builder.Object = nil
+
+	return nil
+}
+
+// Update renovates the existing serviceMeshMemberRoll object with serviceMeshMemberRoll definition in builder.
+func (builder *MemberRollBuilder) Update(force bool) (*MemberRollBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Info("Updating serviceMeshMemberRoll %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	err := builder.apiClient.Update(context.TODO(), builder.Definition)
+
+	if err != nil {
+		if force {
+			glog.V(100).Infof("Failed to update the serviceMeshMemberRoll object %s in namespace %s. "+
+				"Note: Force flag set, executed delete/create methods instead",
+				builder.Definition.Name, builder.Definition.Namespace)
+
+			err := builder.Delete()
+
+			if err != nil {
+				glog.V(100).Infof(
+					"Failed to update the serviceMeshMemberRoll object %s in namespace %s "+
+						"due to error in delete function", builder.Definition.Name, builder.Definition.Namespace)
+
+				return nil, err
+			}
+
+			return builder.Create()
+		}
+	}
+
+	if err == nil {
+		builder.Object = builder.Definition
+	}
+
+	return builder, err
+}
+
+// Exists checks whether the given serviceMeshMemberRoll exists.
+func (builder *MemberRollBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if serviceMeshMemberRoll %s exists in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	builder.Object, err = builder.Get()
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// WithMembersList adds member list section to the ServiceMeshMemberRoleBuilder.
+func (builder *MemberRollBuilder) WithMembersList(membersList []string) *MemberRollBuilder {
+	glog.V(100).Infof("Adding member list %v section to the MemberRollBuilder", membersList)
+
+	if len(membersList) == 0 {
+		glog.V(100).Infof("Cannot add empty membersList to the memberRoll structure")
+
+		builder.errorMsg = "can not modify memberRoll config with empty membersList"
+	}
+
+	if builder.errorMsg != "" {
+		return builder
+	}
+
+	if builder.Definition.Spec.Members == nil {
+		builder.Definition.Spec.Members = membersList
+	} else {
+		builder.Definition.Spec.Members = append(builder.Definition.Spec.Members, membersList...)
+	}
+
+	return builder
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *MemberRollBuilder) validate() (bool, error) {
+	resourceCRD := "ServiceMeshMemberRoll"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		builder.errorMsg = msg.UndefinedCrdObjectErrString(resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
