e87d0469d2428a852febb3ce234ef8f75918435d
Shai Oren <sobarzan@redhat.com>
2023-11-08 20:05:44 +0200

    Addressed Trey's comments

--- pkg/ocm/policylist.go
+++ pkg/ocm/policylist.go
@@ -2,6 +2,7 @@ package ocm
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/golang/glog"
 	"github.com/openshift-kni/eco-goinfra/pkg/clients"
@@ -11,13 +12,27 @@ import (
 
 // ListPoliciesInAllNamespaces returns a cluster-wide policy inventory.
 func ListPoliciesInAllNamespaces(apiClient *clients.Settings,
-	options runtimeclient.ListOptions) (
+	options ...runtimeclient.ListOptions) (
 	[]*PolicyBuilder, error) {
-	glog.V(100).Info("Listing all policies in all namespaces")
+	logMessage := string("Listing all policies in all namespaces")
+	passedOptions := runtimeclient.ListOptions{}
+
+	if len(options) > 1 {
+		glog.V(100).Infof("'options' parameter must be empty or single-valued")
+
+		return nil, fmt.Errorf("error: more than one ListOptions was passed")
+	}
+
+	if len(options) == 1 {
+		passedOptions = options[0]
+		logMessage += fmt.Sprintf(" with the options %v", passedOptions)
+	}
+
+	glog.V(100).Infof(logMessage)
 
 	policyList := &policiesv1.PolicyList{}
 
-	err := apiClient.Client.List(context.Background(), policyList, &options)
+	err := apiClient.Client.List(context.Background(), policyList, &passedOptions)
 
 	if err != nil {
 		glog.V(100).Infof("Failed to list all policies in all namespaces due to %s", err.Error())
