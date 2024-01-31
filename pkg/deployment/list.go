2c5a26ce73028f016a2bc8294f79b9a94c45d7a0
Yurii Prokulevych <yprokule@redhat.com>
2024-01-31 10:28:01 +0100

    ListInAllNamespaces method for Deployments added

--- pkg/deployment/list.go
+++ pkg/deployment/list.go
@@ -56,3 +56,45 @@ func List(apiClient *clients.Settings, nsname string, options ...metaV1.ListOpti
 
 	return deploymentObjects, nil
 }
+
+// ListInAllNamespaces returns deployment inventory in the all the namespaces.
+func ListInAllNamespaces(apiClient *clients.Settings, options ...metaV1.ListOptions) ([]*Builder, error) {
+	passedOptions := metaV1.ListOptions{}
+	logMessage := "Listing deployments in all namespaces"
+
+	if len(options) > 1 {
+		glog.V(100).Infof("'options' parameter must be either empty or single-valued")
+
+		return nil, fmt.Errorf("error: more than one ListOptions was passed")
+	}
+
+	if len(options) == 1 {
+		passedOptions = options[0]
+		logMessage += fmt.Sprintf(" with the options %v", passedOptions)
+	}
+
+	glog.V(100).Infof(logMessage)
+
+	deploymentList, err := apiClient.Deployments("").List(context.Background(), passedOptions)
+
+	if err != nil {
+		glog.V(100).Infof("Failed to list deployments in all namespaces due to %s", err.Error())
+
+		return nil, err
+	}
+
+	var deploymentObjects []*Builder
+
+	for _, runningDeployment := range deploymentList.Items {
+		copiedDeployment := runningDeployment
+		deploymentBuilder := &Builder{
+			apiClient:  apiClient,
+			Object:     &copiedDeployment,
+			Definition: &copiedDeployment,
+		}
+
+		deploymentObjects = append(deploymentObjects, deploymentBuilder)
+	}
+
+	return deploymentObjects, nil
+}
