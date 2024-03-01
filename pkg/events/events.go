7e31e5f2e693be2a6377b449dbab1e1f2701fd83
evgenLevin <76552389+evgenLevin@users.noreply.github.com>
2024-03-01 11:42:09 -0500

    event pkg: Created event pkg (#258)

---
+++ pkg/events/events.go
@@ -0,0 +1,96 @@
+package events
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	k8sv1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Builder provides struct for Event object which contains connection to cluster.
+type Builder struct {
+	// Dynamically discovered Event object.
+	Object *k8sv1.Event
+	// apiClient opens api connection to the cluster.
+	apiClient *clients.Settings
+	// errorMsg used in discovery function before sending api request to cluster.
+	errorMsg string
+}
+
+// Pull pulls existing Event from cluster.
+func Pull(apiClient *clients.Settings, name, nsname string) (*Builder, error) {
+	glog.V(100).Infof("Pulling existing Event name %s under namespace %s from cluster", name, nsname)
+
+	builder := Builder{
+		apiClient: apiClient,
+		Object: &k8sv1.Event{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the Event is empty")
+
+		builder.errorMsg = "Event 'name' cannot be empty"
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The namespace of the Event is empty")
+
+		builder.errorMsg = "Event 'namespace' cannot be empty"
+	}
+
+	if !builder.Exists() {
+		return nil, fmt.Errorf("event object %s doesn't exist in namespace %s", name, nsname)
+	}
+
+	return &builder, nil
+}
+
+// Exists checks whether the given Event exists.
+func (builder *Builder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if Event %s exists", builder.Object.Name)
+
+	var err error
+	builder.Object, err = builder.apiClient.Events(builder.Object.Namespace).
+		Get(context.TODO(), builder.Object.Name, metaV1.GetOptions{})
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *Builder) validate() (bool, error) {
+	resourceCRD := "Event"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
---
+++ pkg/events/list.go
@@ -0,0 +1,56 @@
+package events
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// List returns Events inventory in the given namespace.
+func List(
+	apiClient *clients.Settings, nsname string, options ...metaV1.ListOptions) ([]*Builder, error) {
+	if nsname == "" {
+		glog.V(100).Infof("Events 'nsname' parameter can not be empty")
+
+		return nil, fmt.Errorf("failed to list Events, 'nsname' parameter is empty")
+	}
+
+	logMessage := fmt.Sprintf("Listing Events in the namespace %s", nsname)
+	passedOptions := metaV1.ListOptions{}
+
+	if len(options) > 1 {
+		glog.V(100).Infof("'options' parameter must be empty or single-valued")
+
+		return nil, fmt.Errorf("error: more than one ListOptions was passed")
+	}
+
+	if len(options) == 1 {
+		passedOptions = options[0]
+		logMessage += fmt.Sprintf(" with the options %v", passedOptions)
+	}
+
+	glog.V(100).Infof(logMessage)
+
+	eventList, err := apiClient.Events(nsname).List(context.Background(), passedOptions)
+
+	if err != nil {
+		glog.V(100).Infof("Failed to list Events in the namespace %s due to %s", nsname, err.Error())
+
+		return nil, err
+	}
+
+	var eventObjects []*Builder
+
+	for _, event := range eventList.Items {
+		copiedEvent := event
+		stateBuilder := &Builder{
+			apiClient: apiClient,
+			Object:    &copiedEvent}
+		eventObjects = append(eventObjects, stateBuilder)
+	}
+
+	return eventObjects, nil
+}
