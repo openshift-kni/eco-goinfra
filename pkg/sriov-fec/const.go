c6f7a269da23e0676a04367fd1b51a11268dd0ab
Marius Cornea <mcornea@redhat.com>
2024-03-21 16:29:15 +0200

    Add sriovfec pkg (#264)
    Add sriovfec pkg together with sriov-fec-operator API. This is
    needed since vendoring doesn't work due to older version of
    sigs.k8s.io/controller-runtime pkg[0] than what is used in eco-goinfra.
    
    [0] https://github.com/smart-edge-open/sriov-fec-operator/blob/main/go.mod#L27

---
+++ pkg/sriov-fec/const.go
@@ -0,0 +1,8 @@
+package sriovfec
+
+const (
+	// APIGroup represents sriovfecnodeconfig api group.
+	APIGroup = "sriovfec.intel.com"
+	// APIVersion represents version of sriovfecnodeconfig api.
+	APIVersion = "v2"
+)
