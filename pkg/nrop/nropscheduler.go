7120e6f4b5f18dbc9466d8c96dafd127c324cd34
Elena German <elgerman@redhat.com>
2024-06-18 14:49:59 -0400

    nrop pkg: NUMAResourcesScheduler handler added (#477)
    * bump vendors
    
    * nrop pkg: NUMAResourcesScheduler handler added
    
    * bump vendors
    
    * modify defaultImageSpec

---
+++ pkg/nrop/nropscheduler.go
@@ -0,0 +1,280 @@
+package nrop
+
+import (
+	"context"
+	"fmt"
+
+	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	goclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// SchedulerBuilder provides a struct for NUMAResourcesScheduler object from the cluster
+// and a NUMAResourcesScheduler definition.
+type SchedulerBuilder struct {
+	// NUMAResourcesScheduler definition, used to create the NUMAResourcesScheduler object.
+	Definition *nropv1.NUMAResourcesScheduler
+	// Created NUMAResourcesScheduler object.
+	Object *nropv1.NUMAResourcesScheduler
+	// Used to store latest error message upon defining or mutating NUMAResourcesScheduler definition.
+	errorMsg string
+	// api client to interact with the cluster.
+	apiClient goclient.Client
+}
+
+// NewSchedulerBuilder creates a new instance of NUMAResourcesScheduler.
+func NewSchedulerBuilder(
+	apiClient *clients.Settings, name, nsname string) *SchedulerBuilder {
+	glog.V(100).Infof(
+		"Initializing new NUMAResourcesScheduler structure with the following name: %s in namespace %s",
+		name, nsname)
+
+	if apiClient == nil {
+		glog.V(100).Infof("NUMAResourcesScheduler 'apiClient' cannot be empty")
+
+		return nil
+	}
+
+	builder := &SchedulerBuilder{
+		apiClient: apiClient.Client,
+		Definition: &nropv1.NUMAResourcesScheduler{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the NUMAResourcesScheduler is empty")
+
+		builder.errorMsg = "NUMAResourcesScheduler 'name' cannot be empty"
+
+		return builder
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The nsname of the NUMAResourcesScheduler is empty")
+
+		builder.errorMsg = "NUMAResourcesScheduler 'nsname' cannot be empty"
+
+		return builder
+	}
+
+	return builder
+}
+
+// PullScheduler pulls existing NUMAResourcesScheduler from cluster.
+func PullScheduler(apiClient *clients.Settings, name, nsname string) (*SchedulerBuilder, error) {
+	glog.V(100).Infof("Pulling existing NUMAResourcesScheduler %s in namespace %s from the cluster",
+		name, nsname)
+
+	if apiClient == nil {
+		glog.V(100).Infof("The apiClient is empty")
+
+		return nil, fmt.Errorf("NUMAResourcesScheduler 'apiClient' cannot be empty")
+	}
+
+	builder := SchedulerBuilder{
+		apiClient: apiClient.Client,
+		Definition: &nropv1.NUMAResourcesScheduler{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the NUMAResourcesScheduler is empty")
+
+		return nil, fmt.Errorf("NUMAResourcesScheduler 'name' cannot be empty")
+	}
+
+	if nsname == "" {
+		glog.V(100).Infof("The nsname of the NUMAResourcesScheduler is empty")
+
+		return nil, fmt.Errorf("NUMAResourcesScheduler 'nsname' cannot be empty")
+	}
+
+	if !builder.Exists() {
+		return nil, fmt.Errorf("NUMAResourcesScheduler object %s does not exist", name)
+	}
+
+	builder.Definition = builder.Object
+
+	return &builder, nil
+}
+
+// Get fetches the defined NUMAResourcesScheduler from the cluster.
+func (builder *SchedulerBuilder) Get() (*nropv1.NUMAResourcesScheduler, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Getting NUMAResourcesScheduler %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	nrosObj := &nropv1.NUMAResourcesScheduler{}
+	err := builder.apiClient.Get(context.TODO(), goclient.ObjectKey{
+		Name:      builder.Definition.Name,
+		Namespace: builder.Definition.Namespace,
+	}, nrosObj)
+
+	if err != nil {
+		glog.V(100).Infof("NUMAResourcesScheduler object %s not found in namespace %s: %v",
+			builder.Definition.Name, builder.Definition.Namespace, err)
+
+		return nil, err
+	}
+
+	return nrosObj, nil
+}
+
+// Create makes a NUMAResourcesScheduler in the cluster and stores the created object in struct.
+func (builder *SchedulerBuilder) Create() (*SchedulerBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Creating the NUMAResourcesScheduler %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	if !builder.Exists() {
+		err = builder.apiClient.Create(context.TODO(), builder.Definition)
+		if err == nil {
+			builder.Object = builder.Definition
+		}
+	}
+
+	return builder, err
+}
+
+// Delete removes NUMAResourcesScheduler from a cluster.
+func (builder *SchedulerBuilder) Delete() (*SchedulerBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Deleting the NUMAResourcesScheduler %s from namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	if !builder.Exists() {
+		glog.V(100).Infof("NUMAResourcesScheduler %s cannot be deleted because "+
+			"it does not exist in namespace %s",
+			builder.Definition.Name, builder.Definition.Namespace)
+
+		builder.Object = nil
+
+		return builder, nil
+	}
+
+	err := builder.apiClient.Delete(context.TODO(), builder.Definition)
+
+	if err != nil {
+		return builder, fmt.Errorf("can not delete NUMAResourcesScheduler %s from namespace %s due to %w",
+			builder.Definition.Name, builder.Definition.Namespace, err)
+	}
+
+	builder.Object = nil
+
+	return builder, nil
+}
+
+// Exists checks whether the given NUMAResourcesScheduler exists.
+func (builder *SchedulerBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if NUMAResourcesScheduler %s exists in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	var err error
+	builder.Object, err = builder.Get()
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// Update renovates the existing NUMAResourcesScheduler object with NUMAResourcesScheduler definition in builder.
+func (builder *SchedulerBuilder) Update() (*SchedulerBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return builder, err
+	}
+
+	glog.V(100).Infof("Updating NUMAResourcesScheduler %s in namespace %s",
+		builder.Definition.Name, builder.Definition.Namespace)
+
+	err := builder.apiClient.Update(context.TODO(), builder.Definition)
+
+	if err != nil {
+		glog.V(100).Infof(
+			msg.FailToUpdateError("NUMAResourcesScheduler", builder.Definition.Name))
+
+		return nil, err
+	}
+
+	builder.Object = builder.Definition
+
+	return builder, nil
+}
+
+// WithImageSpec sets the NUMAResourcesScheduler operator's imageSpec.
+func (builder *SchedulerBuilder) WithImageSpec(imageSpec string) *SchedulerBuilder {
+	glog.V(100).Infof("Adding imageSpec to the NUMAResourcesScheduler %s in namespace %s; imageSpec: %s",
+		builder.Definition.Name, builder.Definition.Namespace, imageSpec)
+
+	if valid, _ := builder.validate(); !valid {
+		return builder
+	}
+
+	if imageSpec == "" {
+		glog.V(100).Infof("The 'NUMAResourcesScheduler' imageSpec cannot be empty")
+
+		builder.errorMsg = "can not apply a NUMAResourcesScheduler with an empty imageSpec"
+
+		return builder
+	}
+
+	builder.Definition.Spec.SchedulerImage = imageSpec
+
+	return builder
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *SchedulerBuilder) validate() (bool, error) {
+	resourceCRD := "NUMAResourcesScheduler"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is undefined", resourceCRD)
+
+		return false, fmt.Errorf(msg.UndefinedCrdObjectErrString(resourceCRD))
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		return false, fmt.Errorf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
---
+++ pkg/nrop/nropscheduler_test.go
@@ -0,0 +1,350 @@
+package nrop
+
+import (
+	"fmt"
+	"testing"
+
+	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
+
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var (
+	defaultNROSchedulerName      = "numaresourcesscheduler"
+	defaultNROSchedulerNamespace = "openshift-numaresources"
+	defaultImageSpec             = "test-registry.com/noderesourcetopology-scheduler-image:latest"
+)
+
+func TestNROSchedulerPull(t *testing.T) {
+	generateNROScheduler := func(name, nsname string) *nropv1.NUMAResourcesScheduler {
+		return &nropv1.NUMAResourcesScheduler{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: nsname,
+			},
+		}
+	}
+
+	testCases := []struct {
+		name                string
+		nsname              string
+		addToRuntimeObjects bool
+		expectedError       error
+		client              bool
+	}{
+		{
+			name:                defaultNROSchedulerName,
+			nsname:              defaultNROSchedulerNamespace,
+			addToRuntimeObjects: true,
+			expectedError:       nil,
+			client:              true,
+		},
+		{
+			name:                "",
+			nsname:              defaultNROSchedulerNamespace,
+			addToRuntimeObjects: true,
+			expectedError:       fmt.Errorf("NUMAResourcesScheduler 'name' cannot be empty"),
+			client:              true,
+		},
+		{
+			name:                defaultNROSchedulerName,
+			nsname:              "",
+			addToRuntimeObjects: true,
+			expectedError:       fmt.Errorf("NUMAResourcesScheduler 'nsname' cannot be empty"),
+			client:              true,
+		},
+		{
+			name:                "nrostest",
+			nsname:              defaultNROSchedulerNamespace,
+			addToRuntimeObjects: false,
+			expectedError:       fmt.Errorf("NUMAResourcesScheduler object nrostest does not exist"),
+			client:              true,
+		},
+		{
+			name:                "nrostest",
+			nsname:              defaultNROSchedulerNamespace,
+			addToRuntimeObjects: true,
+			expectedError:       fmt.Errorf("NUMAResourcesScheduler 'apiClient' cannot be empty"),
+			client:              false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		// Pre-populate the runtime objects
+		var runtimeObjects []runtime.Object
+
+		var testSettings *clients.Settings
+
+		testNROS := generateNROScheduler(testCase.name, testCase.nsname)
+
+		if testCase.addToRuntimeObjects {
+			runtimeObjects = append(runtimeObjects, testNROS)
+		}
+
+		if testCase.client {
+			testSettings = clients.GetTestClients(clients.TestClientParams{
+				K8sMockObjects: runtimeObjects,
+			})
+		}
+
+		builderResult, err := PullScheduler(testSettings, testCase.name, testCase.nsname)
+		assert.Equal(t, testCase.expectedError, err)
+
+		if testCase.expectedError != nil {
+			assert.Equal(t, testCase.expectedError.Error(), err.Error())
+		} else {
+			assert.Equal(t, testNROS.Name, builderResult.Object.Name)
+			assert.Equal(t, testNROS.Namespace, builderResult.Object.Namespace)
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestNewNROSchedulerBuilder(t *testing.T) {
+	testCases := []struct {
+		name          string
+		nsname        string
+		expectedError string
+	}{
+		{
+			name:          defaultNROSchedulerName,
+			nsname:        defaultNROSchedulerNamespace,
+			expectedError: "",
+		},
+		{
+			name:          "",
+			nsname:        defaultNROSchedulerNamespace,
+			expectedError: "NUMAResourcesScheduler 'name' cannot be empty",
+		},
+		{
+			name:          defaultNROSchedulerName,
+			nsname:        "",
+			expectedError: "NUMAResourcesScheduler 'nsname' cannot be empty",
+		},
+	}
+
+	for _, testCase := range testCases {
+		testSettings := clients.GetTestClients(clients.TestClientParams{})
+		testNROSBuilder := NewSchedulerBuilder(testSettings, testCase.name, testCase.nsname)
+		assert.NotNil(t, testNROSBuilder.Definition)
+
+		if testCase.expectedError == "" {
+			assert.Equal(t, testCase.name, testNROSBuilder.Definition.Name)
+			assert.Equal(t, testCase.nsname, testNROSBuilder.Definition.Namespace)
+			assert.Equal(t, "", testNROSBuilder.errorMsg)
+		} else {
+			assert.Equal(t, testCase.expectedError, testNROSBuilder.errorMsg)
+		}
+	}
+}
+
+func TestNROSchedulerExists(t *testing.T) {
+	testCases := []struct {
+		testNROS       *SchedulerBuilder
+		expectedStatus bool
+	}{
+		{
+			testNROS:       buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedStatus: true,
+		},
+		{
+			testNROS:       buildInValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedStatus: false,
+		},
+		{
+			testNROS:       buildValidNROSchedulerBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedStatus: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		exist := testCase.testNROS.Exists()
+		assert.Equal(t, testCase.expectedStatus, exist)
+	}
+}
+
+func TestNROSchedulerGet(t *testing.T) {
+	testCases := []struct {
+		testNROS      *SchedulerBuilder
+		expectedError error
+	}{
+		{
+			testNROS:      buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: nil,
+		},
+		{
+			testNROS:      buildInValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: fmt.Errorf("NUMAResourcesScheduler 'name' cannot be empty"),
+		},
+		{
+			testNROS: buildValidNROSchedulerBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: fmt.Errorf("numaresourcesschedulers.nodetopology.openshift.io " +
+				"\"numaresourcesscheduler\" not found"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		nrosObj, err := testCase.testNROS.Get()
+
+		if testCase.expectedError == nil {
+			assert.Equal(t, nrosObj.Name, testCase.testNROS.Definition.Name)
+			assert.Equal(t, nrosObj.Namespace, testCase.testNROS.Definition.Namespace)
+			assert.Nil(t, err)
+		} else {
+			assert.Equal(t, testCase.expectedError.Error(), err.Error())
+		}
+	}
+}
+
+func TestNROSchedulerCreate(t *testing.T) {
+	testCases := []struct {
+		testNROS      *SchedulerBuilder
+		expectedError string
+	}{
+		{
+			testNROS:      buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: "",
+		},
+		{
+			testNROS:      buildInValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: "NUMAResourcesScheduler 'name' cannot be empty",
+		},
+		{
+			testNROS:      buildValidNROSchedulerBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		testNROSBuilder, err := testCase.testNROS.Create()
+
+		if testCase.expectedError == "" {
+			assert.Equal(t, testNROSBuilder.Definition.Name, testNROSBuilder.Object.Name)
+			assert.Equal(t, testNROSBuilder.Definition.Namespace, testNROSBuilder.Object.Namespace)
+			assert.Nil(t, err)
+		} else {
+			assert.Equal(t, testCase.expectedError, err.Error())
+		}
+	}
+}
+
+func TestNROSchedulerDelete(t *testing.T) {
+	testCases := []struct {
+		testNROS      *SchedulerBuilder
+		expectedError error
+	}{
+		{
+			testNROS:      buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: nil,
+		},
+		{
+			testNROS:      buildInValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: fmt.Errorf("NUMAResourcesScheduler 'name' cannot be empty"),
+		},
+		{
+			testNROS:      buildValidNROSchedulerBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		_, err := testCase.testNROS.Delete()
+
+		if testCase.expectedError == nil {
+			assert.Nil(t, testCase.testNROS.Object)
+			assert.Nil(t, err)
+		} else {
+			assert.Equal(t, testCase.expectedError.Error(), err.Error())
+		}
+	}
+}
+
+func TestNROSchedulerUpdate(t *testing.T) {
+	testCases := []struct {
+		testNROS      *SchedulerBuilder
+		expectedError string
+		imageSpec     string
+	}{
+		{
+			testNROS:      buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: "",
+			imageSpec:     defaultImageSpec,
+		},
+		{
+			testNROS:      buildInValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject()),
+			expectedError: "NUMAResourcesScheduler 'name' cannot be empty",
+			imageSpec:     "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		assert.Equal(t, "", testCase.testNROS.Definition.Spec.SchedulerImage)
+		assert.Nil(t, nil, testCase.testNROS.Object)
+		testCase.testNROS.WithImageSpec(testCase.imageSpec)
+		_, err := testCase.testNROS.Update()
+
+		if testCase.expectedError != "" {
+			assert.Equal(t, testCase.expectedError, err.Error())
+		} else {
+			assert.Equal(t, testCase.imageSpec, testCase.testNROS.Definition.Spec.SchedulerImage)
+		}
+	}
+}
+
+func TestNROSchedulerWithImageSpec(t *testing.T) {
+	testCases := []struct {
+		imageSpec      string
+		expectedErrMsg string
+	}{
+		{
+			imageSpec:      defaultImageSpec,
+			expectedErrMsg: "",
+		},
+		{
+			imageSpec:      "",
+			expectedErrMsg: "can not apply a NUMAResourcesScheduler with an empty imageSpec",
+		},
+	}
+
+	for _, testCase := range testCases {
+		testBuilder := buildValidNROSchedulerBuilder(buildNROSchedulerClientWithDummyObject())
+
+		testBuilder.WithImageSpec(testCase.imageSpec)
+
+		assert.Equal(t, testCase.expectedErrMsg, testBuilder.errorMsg)
+
+		if testCase.expectedErrMsg == "" {
+			assert.Equal(t, testCase.imageSpec, testBuilder.Definition.Spec.SchedulerImage)
+		}
+	}
+}
+
+func buildValidNROSchedulerBuilder(apiClient *clients.Settings) *SchedulerBuilder {
+	nrosBuilder := NewSchedulerBuilder(apiClient, defaultNROSchedulerName, defaultNROSchedulerNamespace)
+
+	return nrosBuilder
+}
+
+func buildInValidNROSchedulerBuilder(apiClient *clients.Settings) *SchedulerBuilder {
+	nrosBuilder := NewSchedulerBuilder(apiClient, "", defaultNROSchedulerNamespace)
+
+	return nrosBuilder
+}
+
+func buildNROSchedulerClientWithDummyObject() *clients.Settings {
+	return clients.GetTestClients(clients.TestClientParams{
+		K8sMockObjects: buildDummyNROScheduler(),
+	})
+}
+
+func buildDummyNROScheduler() []runtime.Object {
+	return append([]runtime.Object{}, &nropv1.NUMAResourcesScheduler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      defaultNROSchedulerName,
+			Namespace: defaultNROSchedulerNamespace,
+		},
+	})
+}
