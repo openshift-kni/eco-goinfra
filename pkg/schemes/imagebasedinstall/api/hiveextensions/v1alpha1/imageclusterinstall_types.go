f9a7c7c45d3621418573964684de9462905ce3e3
Trey West <trwest@redhat.com>
2024-07-22 14:01:04 -0400

    Add imageclusterinstall resource and APIs

---
+++ pkg/schemes/imagebasedinstall/api/hiveextensions/v1alpha1/imageclusterinstall_types.go
@@ -0,0 +1,183 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apicfgv1 "github.com/openshift/api/config/v1"
+	hivev1 "github.com/openshift-kni/eco-goinfra/pkg/schemes/imagebasedinstall/hive/api/v1"
+)
+
+const (
+	ImageNotReadyReason = "NotReady"
+	ImageReadyReason    = "Ready"
+	ImageReadyMessage   = "Image is ready for use"
+
+	HostConfiguraionFailedReason      = "HostConfigurationFailed"
+	HostConfiguraionSucceededReason   = "HostConfigurationSucceeded"
+	HostConfigurationSucceededMessage = "Configuration image is attached to the referenced host"
+
+	InstallTimedoutReason  = "ClusterInstallationTimedOut"
+	InstallTimedoutMessage = "Cluster installation has timed out"
+
+	InstallInProgressReason  = "ClusterInstallationInProgress"
+	InstallInProgressMessage = "Cluster installation is in progress"
+
+	InstallSucceededReason  = "ClusterInstallationSucceeded"
+	InstallSucceededMessage = "Cluster installation has succeeded"
+
+	HostValidationFailedReason = "HostValidationFailed"
+	HostValidationSucceeded    = "HostValidationSucceeded"
+	HostValidationPending      = "HostValidationPending"
+	HostValidationsOKMsg       = "The host's validations are passing and image is ready"
+)
+
+// ImageClusterInstallSpec defines the desired state of ImageClusterInstall
+type ImageClusterInstallSpec struct {
+	// ClusterDeploymentRef is a reference to the ClusterDeployment.
+	// +optional
+	ClusterDeploymentRef *corev1.LocalObjectReference `json:"clusterDeploymentRef"`
+
+	// ImageSetRef is a reference to a ClusterImageSet.
+	ImageSetRef hivev1.ClusterImageSetReference `json:"imageSetRef"`
+
+	// ClusterMetadata contains metadata information about the installed cluster.
+	// This must be set as soon as all the information is available.
+	// +optional
+	ClusterMetadata *hivev1.ClusterMetadata `json:"clusterMetadata"`
+
+	// Version is the target OCP version for the cluster
+	// TODO: should this use ImageSetRef?
+	Version string `json:"version,omitempty"`
+
+	// NodeIP is the desired IP for the host
+	// +optional
+	// Deprecated: this field is ignored (will be removed in a future release).
+	NodeIP string `json:"nodeIP,omitempty"`
+
+	// Hostname is the desired hostname for the host
+	Hostname string `json:"hostname,omitempty"`
+
+	// SSHKey is the public Secure Shell (SSH) key to provide access to
+	// instances. Equivalent to install-config.yaml's sshKey.
+	// This key will be added to the host to allow ssh access
+	SSHKey string `json:"sshKey,omitempty"`
+
+	// ImageDigestSources lists sources/repositories for the release-image content.
+	// +optional
+	ImageDigestSources []apicfgv1.ImageDigestMirrors `json:"imageDigestSources,omitempty"`
+
+	// CABundle is a reference to a config map containing the new bundle of trusted certificates for the host.
+	// The tls-ca-bundle.pem entry in the config map will be written to /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
+	CABundleRef *corev1.LocalObjectReference `json:"caBundleRef,omitempty"`
+
+	// NetworkConfigRef is the reference to a config map containing network configuration files if necessary.
+	// Keys should be of the form *.nmconnection and each represent an nmconnection file to be applied to the host.
+	// +optional
+	NetworkConfigRef *corev1.LocalObjectReference `json:"networkConfigRef,omitempty"`
+
+	// ExtraManifestsRefs is list of config map references containing additional manifests to be applied to the relocated cluster.
+	// +optional
+	ExtraManifestsRefs []corev1.LocalObjectReference `json:"extraManifestsRef,omitempty"`
+
+	// BareMetalHostRef identifies a BareMetalHost object to be used to attach the configuration to the host.
+	// +optional
+	BareMetalHostRef *BareMetalHostReference `json:"bareMetalHostRef,omitempty"`
+
+	// MachineNetwork is the subnet provided by user for the ocp cluster.
+	// This will be used to create the node network and choose ip address for the node.
+	// Equivalent to install-config.yaml's machineNetwork.
+	// +optional.
+	MachineNetwork string `json:"machineNetwork,omitempty"`
+
+	// Proxy defines the proxy settings to be applied in relocated cluster
+	// +optional
+	Proxy *Proxy `json:"proxy,omitempty"`
+}
+
+// ImageClusterInstallStatus defines the observed state of ImageClusterInstall
+type ImageClusterInstallStatus struct {
+	// Conditions is a list of conditions associated with syncing to the cluster.
+	// +optional
+	Conditions []hivev1.ClusterInstallCondition `json:"conditions,omitempty"`
+
+	// InstallRestarts is the total count of container restarts on the clusters install job.
+	InstallRestarts int `json:"installRestarts,omitempty"`
+
+	BareMetalHostRef *BareMetalHostReference `json:"bareMetalHostRef,omitempty"`
+
+	// ConfigurationImageURL is the externally accessible URL for downloading the image containing the SNO configuration
+	ConfigurationImageURL string `json:"configurationImageURL,omitempty"`
+
+	// BootTime indicates the time at which the host was requested to boot. Used to determine install timeouts.
+	BootTime metav1.Time `json:"bootTime,omitempty"`
+}
+
+type BareMetalHostReference struct {
+	// Name identifies the BareMetalHost within a namespace
+	Name string `json:"name"`
+	// Namespace identifies the namespace containing the referenced BareMetalHost
+	Namespace string `json:"namespace"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="RequirementsMet",type="string",JSONPath=".status.conditions[?(@.type=='RequirementsMet')].reason"
+// +kubebuilder:printcolumn:name="Completed",type="string",JSONPath=".status.conditions[?(@.type=='Completed')].reason"
+// +kubebuilder:printcolumn:name="ConfigurationImageURL",type="string",JSONPath=".status.configurationImageURL"
+// +kubebuilder:printcolumn:name="BareMetalHostRef",type="string",JSONPath=".spec.bareMetalHostRef.name"
+
+// ImageClusterInstall is the Schema for the imageclusterinstall API
+type ImageClusterInstall struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ImageClusterInstallSpec   `json:"spec,omitempty"`
+	Status ImageClusterInstallStatus `json:"status,omitempty"`
+}
+
+// Proxy defines the proxy settings for the cluster.
+// At least one of HTTPProxy or HTTPSProxy is required.
+type Proxy struct {
+	// HTTPProxy is the URL of the proxy for HTTP requests.
+	// +optional
+	HTTPProxy string `json:"httpProxy,omitempty"`
+
+	// HTTPSProxy is the URL of the proxy for HTTPS requests.
+	// +optional
+	HTTPSProxy string `json:"httpsProxy,omitempty"`
+
+	// NoProxy is a comma-separated list of domains and CIDRs for which the proxy should not be
+	// used.
+	// +optional
+	NoProxy string `json:"noProxy,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ImageClusterInstallList contains a list of ImageClusterInstall
+type ImageClusterInstallList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ImageClusterInstall `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ImageClusterInstall{}, &ImageClusterInstallList{})
+}
---
+++ pkg/schemes/imagebasedinstall/api/hiveextensions/v1alpha1/imageclusterinstall_webhook.go
@@ -0,0 +1,160 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"reflect"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// log is for logging in this package.
+var icilog = logf.Log.WithName("imageclusterinstall-resource")
+
+func (r *ImageClusterInstall) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+var _ webhook.Validator = &ImageClusterInstall{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *ImageClusterInstall) ValidateCreate() (admission.Warnings, error) {
+	icilog.Info("validate create", "name", r.Name)
+	if err := r.validate(); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (r *ImageClusterInstall) validate() error {
+	if err := isValidSSHPublicKey(r.Spec.SSHKey); err != nil {
+		return fmt.Errorf("invalid ssh key: %v", err)
+	}
+	if err := isValidHostname(r.Spec.Hostname); err != nil {
+		return fmt.Errorf("invalid hostname: %w", err)
+	}
+
+	if err := isValidMachineNetwork(r.Spec.MachineNetwork); err != nil {
+		return fmt.Errorf("invalid machine network: %w", err)
+	}
+
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *ImageClusterInstall) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
+	icilog.Info("validate update", "name", r.Name)
+
+	if err := r.validate(); err != nil {
+		return nil, err
+	}
+
+	oldClusterInstall, ok := old.(*ImageClusterInstall)
+	if !ok {
+		return nil, fmt.Errorf("old object is not an ImageClusterInstall")
+	}
+
+	if oldClusterInstall.Spec.BareMetalHostRef == nil && r.Spec.BareMetalHostRef == nil {
+		return nil, nil
+	}
+
+	// Allow update if it's not the spec
+	if !isSpecUpdate(oldClusterInstall, r) {
+		return nil, nil
+	}
+	if BMHRefsMatch(oldClusterInstall.Spec.BareMetalHostRef, r.Spec.BareMetalHostRef) {
+		return nil, fmt.Errorf("Cannot update ImageClusterInstall when BareMetalHostRef is set, unset BareMetalHostRef before making changes")
+	}
+	return nil, nil
+}
+
+func isSpecUpdate(oldClusterInstall *ImageClusterInstall, newClusterInstall *ImageClusterInstall) bool {
+	oldSpec := oldClusterInstall.Spec.DeepCopy()
+	newSpec := newClusterInstall.Spec.DeepCopy()
+	oldSpec.ClusterMetadata = nil
+	newSpec.ClusterMetadata = nil
+
+	return !reflect.DeepEqual(oldSpec, newSpec)
+}
+
+func isValidSSHPublicKey(pubKeyString string) error {
+	if pubKeyString == "" {
+		return nil
+	}
+	// Trim any leading/trailing whitespaces
+	pubKeyString = strings.TrimSpace(pubKeyString)
+	pubKeyBytes := []byte(pubKeyString)
+	_, _, _, _, err := ssh.ParseAuthorizedKey(pubKeyBytes)
+	if err != nil {
+		return fmt.Errorf("error parsing public key: %w", err)
+	}
+	// If parsing is successful, the key is valid
+	return nil
+}
+
+func isValidHostname(hostname string) error {
+	if hostname == "" {
+		return nil
+	}
+	errs := validation.IsDNS1123Subdomain(hostname)
+	if len(errs) != 0 {
+		return errors.New(strings.Join(errs, ";"))
+	}
+	return nil
+}
+
+func isValidMachineNetwork(machineNetwork string) error {
+	if machineNetwork == "" {
+		return nil
+	}
+
+	_, _, err := net.ParseCIDR(machineNetwork)
+	if err != nil {
+		return fmt.Errorf("error parsing machine network, check that it is valid cidr: %w", err)
+	}
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *ImageClusterInstall) ValidateDelete() (admission.Warnings, error) {
+	return nil, nil
+}
+
+func BMHRefsMatch(ref1 *BareMetalHostReference, ref2 *BareMetalHostReference) bool {
+	if ref1 == nil && ref2 == nil {
+		return true
+	}
+
+	if ref1 == nil && ref2 != nil || ref1 != nil && ref2 == nil {
+		return false
+	}
+
+	return *ref1 == *ref2
+}
