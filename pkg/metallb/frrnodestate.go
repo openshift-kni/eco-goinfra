743de61ccd23f349b4e3cd63085a6c7ab9d68db5
Gregory Kopels <gkopels@gkopels-thinkpadx1carbongen9.raanana.csb>
2024-09-20 15:45:28 +0300

    cnf network: add frrNodeState

---
+++ pkg/metallb/frrnodestate.go
@@ -0,0 +1,125 @@
+package metallb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/schemes/metallb/frrtypes"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// FrrNodeStateBuilder provides struct for FrrNodeState object which contains connection to cluster and
+// frrconfiguration definitions.
+type FrrNodeStateBuilder struct {
+	Definition *frrtypes.FRRNodeState
+	Object     *frrtypes.FRRNodeState
+	apiClient  runtimeClient.Client
+	errorMsg   string
+}
+
+// PullFrrNodeState retrieves an existing FrrNodeState object from the cluster.
+func PullFrrNodeState(apiClient *clients.Settings, name string) (*FrrNodeStateBuilder, error) {
+	glog.V(100).Infof("Pulling FrrNodeState object name:%s", name)
+
+	if apiClient == nil {
+		glog.V(100).Infof("The apiClient cannot be nil")
+
+		return nil, fmt.Errorf("the apiClient cannot be nil")
+	}
+
+	err := apiClient.AttachScheme(frrtypes.AddToScheme)
+	if err != nil {
+		glog.V(100).Infof("Failed to add FrrNodeState scheme to client schemes")
+
+		return nil, err
+	}
+
+	frrStateBuilder := FrrNodeStateBuilder{
+		apiClient: apiClient.Client,
+		Definition: &frrtypes.FRRNodeState{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Infof("The name of the FrrNodeState is empty")
+
+		return nil, fmt.Errorf("frrNodeState 'name' cannot be empty")
+	}
+
+	if !frrStateBuilder.Exists() {
+		return nil, fmt.Errorf("frrNodeState object %s does not exist", name)
+	}
+
+	frrStateBuilder.Definition = frrStateBuilder.Object
+
+	return &frrStateBuilder, nil
+}
+
+// Exists checks whether the given FRRNodeState exists.
+func (builder *FrrNodeStateBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if FrrNodeState %s exists", builder.Definition.Name)
+
+	var err error
+	builder.Object, err = builder.Get()
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// Get returns FrrNodeState object if found.
+func (builder *FrrNodeStateBuilder) Get() (*frrtypes.FRRNodeState, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Collecting FrrNodeState object %s", builder.Definition.Name)
+
+	frrNodeState := &frrtypes.FRRNodeState{}
+	err := builder.apiClient.Get(context.TODO(), runtimeClient.ObjectKey{
+		Name: builder.Definition.Name,
+	}, frrNodeState)
+
+	if err != nil {
+		glog.V(100).Infof("FrrNodeState object %s does not exist", builder.Definition.Name)
+
+		return nil, err
+	}
+
+	return frrNodeState, nil
+}
+
+// validate will check that the builder and builder definition are properly initialized before
+// accessing any member fields.
+func (builder *FrrNodeStateBuilder) validate() (bool, error) {
+	resourceCRD := "frrnodestate"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
+
+		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message: %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
---
+++ pkg/metallb/frrnodestate_test.go
@@ -0,0 +1,186 @@
+package metallb
+
+import (
+	"testing"
+
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/schemes/metallb/frrtypes"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var (
+	defaultNodeName = "worker-0"
+)
+
+func TestFrrNodeStateGet(t *testing.T) {
+	var runtimeObjects []runtime.Object
+	testCases := []struct {
+		testFrrNodeState    *FrrNodeStateBuilder
+		addToRuntimeObjects bool
+		expectedError       string
+		client              bool
+	}{
+		{
+			testFrrNodeState: buildValidFrrNodeStateTestBuilder(buildTestFrrClientWithDummyNode(defaultNodeName)),
+			expectedError:    "",
+		},
+		{
+			testFrrNodeState: buildValidFrrNodeStateTestBuilder(clients.GetTestClients(clients.TestClientParams{
+				K8sMockObjects:  runtimeObjects,
+				SchemeAttachers: frrTestSchemes,
+			})),
+			expectedError: "frrnodestates.frrk8s.metallb.io \"worker-0\" not found",
+		},
+	}
+
+	for _, testCase := range testCases {
+		frrNodeState, err := testCase.testFrrNodeState.Get()
+
+		if testCase.expectedError == "" {
+			assert.Nil(t, err)
+			assert.Equal(t, frrNodeState.Name, testCase.testFrrNodeState.Definition.Name, frrNodeState.Name)
+		} else {
+			assert.EqualError(t, err, testCase.expectedError)
+		}
+	}
+}
+
+func TestFrrNodeStateExist(t *testing.T) {
+	testCases := []struct {
+		testFrrNodeState *FrrNodeStateBuilder
+		exist            bool
+	}{
+		{
+			testFrrNodeState: buildValidFrrNodeStateTestBuilder(buildTestFrrClientWithDummyNode("test-node")),
+			exist:            false,
+		},
+		{
+			testFrrNodeState: buildValidFrrNodeStateTestBuilder(buildTestFrrClientWithDummyNode(defaultNodeName)),
+			exist:            true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		exist := testCase.testFrrNodeState.Exists()
+		assert.Equal(t, testCase.exist, exist)
+	}
+}
+
+func TestPullFrrNodeState(t *testing.T) {
+	generateFrrNodeState := func(name string) *frrtypes.FRRNodeState {
+		return &frrtypes.FRRNodeState{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Status: frrtypes.FRRNodeStateStatus{},
+		}
+	}
+
+	testCases := []struct {
+		name                string
+		expectedError       bool
+		addToRuntimeObjects bool
+		expectedErrorText   string
+		client              bool
+	}{
+		{
+			name:                "test1",
+			expectedError:       false,
+			addToRuntimeObjects: true,
+			client:              true,
+		},
+		{
+			name:                "",
+			expectedError:       true,
+			expectedErrorText:   "frrNodeState 'name' cannot be empty",
+			addToRuntimeObjects: true,
+			client:              true,
+		},
+		{
+			name:                "test1",
+			expectedError:       true,
+			expectedErrorText:   "frrNodeState object test1 does not exist",
+			addToRuntimeObjects: false,
+			client:              true,
+		},
+		{
+			name:                "test1",
+			expectedError:       true,
+			expectedErrorText:   "the apiClient cannot be nil",
+			addToRuntimeObjects: true,
+			client:              false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		// Pre-populate the runtime objects
+		var runtimeObjects []runtime.Object
+
+		var testSettings *clients.Settings
+
+		testNodeNetState := generateFrrNodeState(testCase.name)
+
+		if testCase.addToRuntimeObjects {
+			runtimeObjects = append(runtimeObjects, testNodeNetState)
+		}
+
+		if testCase.client {
+			testSettings = clients.GetTestClients(clients.TestClientParams{
+				K8sMockObjects:  runtimeObjects,
+				SchemeAttachers: frrTestSchemes,
+			})
+		}
+
+		// Test the Pull method
+		builderResult, err := PullFrrNodeState(testSettings, testCase.name)
+
+		// Check the error
+		if testCase.expectedError {
+			assert.NotNil(t, err)
+
+			// Check the error message
+			if testCase.expectedErrorText != "" {
+				assert.Equal(t, testCase.expectedErrorText, err.Error())
+			}
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, testNodeNetState.Name, builderResult.Object.Name)
+		}
+	}
+}
+
+// buildDummyNode returns a Node with the provided name.
+func buildDummyFRRNodeState(name string) *frrtypes.FRRNodeState {
+	return &frrtypes.FRRNodeState{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+}
+
+// buildTestFrrClientWithDummyNode returns a client with a dummy node.
+func buildTestFrrClientWithDummyNode(nodeName string) *clients.Settings {
+	return clients.GetTestClients(clients.TestClientParams{
+		K8sMockObjects:  []runtime.Object{buildDummyFRRNodeState(nodeName)},
+		SchemeAttachers: frrTestSchemes,
+	})
+}
+
+func buildValidFrrNodeStateTestBuilder(apiClient *clients.Settings) *FrrNodeStateBuilder {
+	return newFRRNodeStateBuilder(apiClient, defaultNodeName)
+}
+
+func newFRRNodeStateBuilder(apiClient *clients.Settings, frr string) *FrrNodeStateBuilder {
+	if apiClient == nil {
+		return nil
+	}
+
+	builder := FrrNodeStateBuilder{
+		apiClient:  apiClient.Client,
+		Definition: buildDummyFRRNodeState(frr),
+	}
+
+	return &builder
+}
