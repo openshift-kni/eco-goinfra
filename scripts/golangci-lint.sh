80fa1430a41b710b9ece1c2bbb7cc51d25ec3efa
nkononov <nkononov@redhat.com>
2024-10-07 17:24:00 -0400

    ci: upgrade golangci-lint ver from 1.60.3=>1.61.0

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -4,7 +4,7 @@ set -e
 
 . "$(dirname "$0")"/common.sh
 
-GOLANGCI_LINT_VERSION="1.60.3"
+GOLANGCI_LINT_VERSION="1.61.0"
 
 # IsGoLangCiLintInstalled is used to check whether golangci-lint executable is on the $PATH.
 function IsGolangCiLintInstalled() {
