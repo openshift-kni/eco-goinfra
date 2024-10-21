fc5ce6ff1bc7b85ba35c258e858d38463bf27e8e
Brandon Palm <bpalm@redhat.com>
2024-10-21 16:38:56 -0500

    velero: adjust delete() returns

--- pkg/velero/backup.go
+++ pkg/velero/backup.go
@@ -355,7 +355,12 @@ func (builder *BackupBuilder) Delete() (*BackupBuilder, error) {
 		builder.Definition.Name, builder.Definition.Namespace)
 
 	if !builder.Exists() {
-		return builder, fmt.Errorf("backup cannot be deleted because it does not exist")
+		glog.V(100).Infof("Backup %s in namespace %s cannot be deleted because it does not exist",
+			builder.Definition.Name, builder.Definition.Namespace)
+
+		builder.Object = nil
+
+		return builder, nil
 	}
 
 	err := builder.apiClient.Delete(context.TODO(), builder.Definition)
