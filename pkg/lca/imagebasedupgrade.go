5d2afce96999598147d91eec80aca10be57d3e38
Brandon Palm <bpalm@redhat.com>
2024-10-21 11:24:31 -0500

    lca imagebasedupgrade: adjust delete() returns (#742)

--- pkg/lca/imagebasedupgrade.go
+++ pkg/lca/imagebasedupgrade.go
@@ -170,7 +170,12 @@ func (builder *ImageBasedUpgradeBuilder) Delete() (*ImageBasedUpgradeBuilder, er
 		builder.Definition.Name)
 
 	if !builder.Exists() {
-		return builder, fmt.Errorf("imagebasedupgrade cannot be deleted because it does not exist")
+		glog.V(100).Infof("imagebasedupgrade %s cannot be deleted because it does not exist",
+			builder.Definition.Name)
+
+		builder.Object = nil
+
+		return builder, nil
 	}
 
 	err := builder.apiClient.Delete(context.TODO(), builder.Definition)
