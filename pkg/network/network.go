52736615a5e91d0f5b5a04b188b2b1ea9ec93c9e
Kirsten Laskoski <klaskosk@redhat.com>
2024-11-01 10:38:53 -0400

    network: add unit tests and switch to go client (#765)
    * Add unit tests for network.go in the network package.
    * Switch network.go to use the runtime client and add the Get method.
    * Adjust naming to match the operator.go convention.

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -10,6 +10,7 @@ import (
 	configv1 "github.com/openshift/api/config/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -23,15 +24,28 @@ type ConfigBuilder struct {
 	// Created network object.
 	Object *configv1.Network
 	// api client to interact with the cluster.
-	apiClient *clients.Settings
+	apiClient runtimeclient.Client
 }
 
 // PullConfig loads an existing network into ConfigBuilder struct.
 func PullConfig(apiClient *clients.Settings) (*ConfigBuilder, error) {
-	glog.V(100).Infof("Pulling existing network name: %s", clusterNetworkName)
+	glog.V(100).Infof("Pulling existing network.config name: %s", clusterNetworkName)
 
-	builder := ConfigBuilder{
-		apiClient: apiClient,
+	if apiClient == nil {
+		glog.V(100).Info("The network.configapiClient is nil")
+
+		return nil, fmt.Errorf("network.config 'apiClient' cannot be nil")
+	}
+
+	err := apiClient.AttachScheme(configv1.Install)
+	if err != nil {
+		glog.V(100).Info("Failed to add config v1 scheme to client schemes")
+
+		return nil, err
+	}
+
+	builder := &ConfigBuilder{
+		apiClient: apiClient.Client,
 		Definition: &configv1.Network{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: clusterNetworkName,
@@ -40,12 +54,34 @@ func PullConfig(apiClient *clients.Settings) (*ConfigBuilder, error) {
 	}
 
 	if !builder.Exists() {
-		return nil, fmt.Errorf("network object %s does not exist", clusterNetworkName)
+		glog.V(100).Infof("network.config %s does not exist", clusterNetworkName)
+
+		return nil, fmt.Errorf("network.config object %s does not exist", clusterNetworkName)
 	}
 
 	builder.Definition = builder.Object
 
-	return &builder, nil
+	return builder, nil
+}
+
+// Get returns the network object from the cluster if it exists.
+func (builder *ConfigBuilder) Get() (*configv1.Network, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Getting network.config object %s", builder.Definition.Name)
+
+	network := &configv1.Network{}
+	err := builder.apiClient.Get(context.TODO(), runtimeclient.ObjectKey{Name: builder.Definition.Name}, network)
+
+	if err != nil {
+		glog.V(100).Infof("Failed to get network.config object %s: %v", builder.Definition.Name, err)
+
+		return nil, err
+	}
+
+	return network, err
 }
 
 // Exists checks whether the given network exists.
@@ -54,13 +90,10 @@ func (builder *ConfigBuilder) Exists() bool {
 		return false
 	}
 
-	glog.V(100).Infof(
-		"Checking if network %s exists",
-		builder.Definition.Name)
+	glog.V(100).Infof("Checking if network.config %s exists", builder.Definition.Name)
 
 	var err error
-	builder.Object, err = builder.apiClient.ConfigV1Interface.Networks().Get(
-		context.TODO(), builder.Definition.Name, metav1.GetOptions{})
+	builder.Object, err = builder.Get()
 
 	return err == nil || !k8serrors.IsNotFound(err)
 }
@@ -68,7 +101,7 @@ func (builder *ConfigBuilder) Exists() bool {
 // validate will check that the builder and builder definition are properly initialized before
 // accessing any member fields.
 func (builder *ConfigBuilder) validate() (bool, error) {
-	resourceCRD := "Network.Config"
+	resourceCRD := "network.config"
 
 	if builder == nil {
 		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
