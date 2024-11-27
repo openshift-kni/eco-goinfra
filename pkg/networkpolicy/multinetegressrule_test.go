3d240e19c94993e2f6b85f1e4e73164eb207aa25
Anvesh Jaggapatruni <ajaggapa@redhat.com>
2024-11-27 22:30:22 +0530

    networkpolicy pkg: add new methods to multi network policy (#792)

--- pkg/networkpolicy/multinetegressrule_test.go
+++ pkg/networkpolicy/multinetegressrule_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,6 +27,46 @@ func TestEgressWithPortAndProtocol(t *testing.T) {
 	builder.WithPortAndProtocol(0, "TCP")
 
 	assert.Equal(t, builder.errorMsg, "port number can not be 0")
+}
+
+func TestEgressWithProtocol(t *testing.T) {
+	testCases := []struct {
+		protocol      v1.Protocol
+		expectedError string
+	}{
+		{protocol: v1.ProtocolTCP, expectedError: ""},
+		{protocol: v1.ProtocolUDP, expectedError: ""},
+		{protocol: v1.ProtocolSCTP, expectedError: ""},
+		{protocol: "dummy", expectedError: "invalid protocol argument. Allowed protocols: TCP, UDP & SCTP"},
+	}
+	for _, testCase := range testCases {
+		builder := NewEgressRuleBuilder().WithProtocol(testCase.protocol)
+		assert.Equal(t, testCase.expectedError, builder.errorMsg)
+
+		if testCase.expectedError == "" {
+			assert.Equal(t, testCase.protocol, *builder.definition.Ports[0].Protocol)
+		}
+	}
+}
+
+func TestEgressWithPort(t *testing.T) {
+	testCases := []struct {
+		port          uint16
+		expectedError string
+	}{
+		{
+			port:          5001,
+			expectedError: "",
+		},
+		{
+			port:          0,
+			expectedError: "port number cannot be 0",
+		},
+	}
+	for _, testCase := range testCases {
+		builder := NewEgressRuleBuilder().WithPort(testCase.port)
+		assert.Equal(t, testCase.expectedError, builder.errorMsg)
+	}
 }
 
 func TestEgressWithOptions(t *testing.T) {
@@ -84,6 +125,73 @@ func TestEgressWithPeerPodSelector(t *testing.T) {
 	assert.Len(t, builder.definition.To, 1)
 }
 
+func TestEgressWithPeerNamespaceSelector(t *testing.T) {
+	testCases := []struct {
+		namespaceSelector metav1.LabelSelector
+		expectedError     string
+	}{
+		{
+			namespaceSelector: metav1.LabelSelector{MatchLabels: map[string]string{"app": "nginx"}},
+			expectedError:     "",
+		},
+		{
+			namespaceSelector: metav1.LabelSelector{MatchExpressions: []metav1.LabelSelectorRequirement{
+				{
+					Key:      "app",
+					Operator: metav1.LabelSelectorOpIn,
+					Values:   []string{"nginx"},
+				},
+			}},
+			expectedError: "",
+		},
+	}
+	for _, testCase := range testCases {
+		builder := NewEgressRuleBuilder().WithPeerNamespaceSelector(testCase.namespaceSelector)
+		assert.Equal(t, testCase.expectedError, builder.errorMsg)
+		assert.Equal(t, &testCase.namespaceSelector, builder.definition.To[0].NamespaceSelector)
+	}
+}
+
+func TestEgressWithCIDR(t *testing.T) {
+	testCases := []struct {
+		cidr           string
+		except         string
+		expectedLength int
+	}{
+		{
+			cidr:           "192.168.1.1/24",
+			expectedLength: 1,
+		},
+		{
+			cidr:           "192.168.1.1",
+			expectedLength: 0,
+		},
+		{
+			cidr:           "192.168.1.1/24",
+			except:         "192.168.1.10/32",
+			expectedLength: 1,
+		},
+	}
+	for _, testCase := range testCases {
+		if len(testCase.except) != 0 {
+			builder := NewEgressRuleBuilder().WithCIDR(testCase.cidr, []string{testCase.except})
+			assert.Equal(t, testCase.expectedLength, len(builder.definition.To))
+			assert.Equal(t, testCase.except, builder.definition.To[0].IPBlock.Except[0])
+
+			if len(builder.definition.To) != 0 {
+				assert.Equal(t, testCase.cidr, builder.definition.To[0].IPBlock.CIDR)
+			}
+		} else {
+			builder := NewEgressRuleBuilder().WithCIDR(testCase.cidr)
+			assert.Equal(t, testCase.expectedLength, len(builder.definition.To))
+
+			if len(builder.definition.To) != 0 {
+				assert.Equal(t, testCase.cidr, builder.definition.To[0].IPBlock.CIDR)
+			}
+		}
+	}
+}
+
 func TestEgressWithPeerPodSelectorAndCIDR(t *testing.T) {
 	builder := NewEgressRuleBuilder()
 
@@ -114,6 +222,30 @@ func TestEgressWithPeerPodSelectorAndCIDR(t *testing.T) {
 	}, "192.168.1.1/24", []string{"192.168.1.1"})
 
 	assert.Equal(t, builder.definition.To[0].IPBlock.Except[0], "192.168.1.1")
+}
+
+func TestEgressWithPeerPodAndNamespaceSelector(t *testing.T) {
+	testCases := []struct {
+		podSelector       metav1.LabelSelector
+		namespaceSelector metav1.LabelSelector
+		expectedError     string
+	}{
+		{
+			podSelector: metav1.LabelSelector{MatchLabels: map[string]string{"app": "nginx"}},
+			namespaceSelector: metav1.LabelSelector{MatchExpressions: []metav1.LabelSelectorRequirement{{
+				Key:      "app",
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{"nginx"},
+			}}},
+			expectedError: "",
+		},
+	}
+	for _, testCase := range testCases {
+		builder := NewEgressRuleBuilder().WithPeerPodAndNamespaceSelector(testCase.podSelector, testCase.namespaceSelector)
+		assert.Equal(t, testCase.expectedError, builder.errorMsg)
+		assert.Equal(t, &testCase.podSelector, builder.definition.To[0].PodSelector)
+		assert.Equal(t, &testCase.namespaceSelector, builder.definition.To[0].NamespaceSelector)
+	}
 }
 
 func TestEgressGetEgressRuleCfg(t *testing.T) {
