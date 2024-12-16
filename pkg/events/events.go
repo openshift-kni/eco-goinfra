b70bc946e2c38235a87a6290b6db777874006f85
Brandon Palm <bpalm@redhat.com>
2024-12-16 14:35:51 -0600

    events: adjust return consistency

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -90,7 +90,7 @@ func (builder *Builder) validate() (bool, error) {
 	if builder.apiClient == nil {
 		glog.V(100).Infof("The %s builder apiclient is nil", resourceCRD)
 
-		builder.errorMsg = fmt.Sprintf("%s builder cannot have nil apiClient", resourceCRD)
+		return false, fmt.Errorf("%s builder cannot have nil apiClient", resourceCRD)
 	}
 
 	if builder.errorMsg != "" {
