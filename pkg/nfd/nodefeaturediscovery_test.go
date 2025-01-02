bbdd355be47135f891cf344b50c4272ff0d67248
Brandon Palm <bpalm@redhat.com>
2025-01-02 15:22:10 -0600

    nfd: adjust return consistency

--- pkg/nfd/nodefeaturediscovery_test.go
+++ pkg/nfd/nodefeaturediscovery_test.go
@@ -48,12 +48,13 @@ func TestNFDNewBuilderFromObjectString(t *testing.T) {
 		{
 			almString:         "",
 			client:            true,
-			expectedErrorText: "NodeFeatureDiscovery definition is nil",
+			expectedErrorText: "Error initializing NodeFeatureDiscovery from alm-examples: almExample is an empty string",
 		},
 		{
-			almString:         "{invalid}",
-			client:            true,
-			expectedErrorText: "NodeFeatureDiscovery definition is nil",
+			almString: "{invalid}",
+			client:    true,
+			//nolint:lll
+			expectedErrorText: "Error initializing NodeFeatureDiscovery from alm-examples: invalid character 'i' looking for beginning of object key string",
 		},
 		{
 			almString:         almExample,
