b02c2ff10243927c38102d665d08dd6073694387
Kirsten Laskoski <klaskosk@redhat.com>
2025-02-11 10:32:09 -0500

    ocm: add Klusterlet resource and unit tests (#924)
    This PR adds the Klusterlet resource to the ocm package along with unit tests for it. The original motivation is for spoke clusters where the ClusterName field is the most reliable way to get the name of the cluster and its ManagedCluster resource.

---
+++ pkg/ocm/klusterlet.go
@@ -0,0 +1,250 @@
+package ocm
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/msg"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	operatorv1 "open-cluster-management.io/api/operator/v1"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// KlusterletName is the name of the klusterlet created by importing a ManagedCluster.
+const KlusterletName = "klusterlet"
+
+// KlusterletBuilder provides a struct to interface with Klusterlet resources on a specific cluster.
+type KlusterletBuilder struct {
+	// Definition of the Klusterlet used to create the resource.
+	Definition *operatorv1.Klusterlet
+	// Object of the Klusterlet as it is on the cluster.
+	Object *operatorv1.Klusterlet
+	// apiClient used to interact with the cluster.
+	apiClient runtimeclient.Client
+	// errorMsg is used to store the error message from functions that do not return an error.
+	errorMsg string
+}
+
+// NewKlusterletBuilder creates a new instance of a Klusterlet builder.
+func NewKlusterletBuilder(apiClient *clients.Settings, name string) *KlusterletBuilder {
+	glog.V(100).Infof("Initializing new Klusterlet structure with the following params: name: %s", name)
+
+	if apiClient == nil {
+		glog.V(100).Infof("The apiClient of the Klusterlet is nil")
+
+		return nil
+	}
+
+	err := apiClient.AttachScheme(operatorv1.Install)
+	if err != nil {
+		glog.V(100).Infof("Failed to add ocm operator v1 scheme to client schemes: %v", err)
+
+		return nil
+	}
+
+	builder := &KlusterletBuilder{
+		apiClient: apiClient.Client,
+		Definition: &operatorv1.Klusterlet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Info("The name of the Klusterlet is empty")
+
+		builder.errorMsg = "klusterlet 'name' cannot be empty"
+
+		return builder
+	}
+
+	return builder
+}
+
+// PullKlusterlet pulls an existing Klusterlet into a Builder struct.
+func PullKlusterlet(apiClient *clients.Settings, name string) (*KlusterletBuilder, error) {
+	glog.V(100).Infof("Pulling existing Klusterlet %s from cluster", name)
+
+	if apiClient == nil {
+		glog.V(100).Infof("The apiClient of the Klusterlet is nil")
+
+		return nil, fmt.Errorf("klusterlet 'apiClient' cannot be nil")
+	}
+
+	err := apiClient.AttachScheme(operatorv1.Install)
+	if err != nil {
+		glog.V(100).Infof("Failed to add ocm operator v1 scheme to client schemes: %v", err)
+
+		return nil, err
+	}
+
+	builder := &KlusterletBuilder{
+		apiClient: apiClient.Client,
+		Definition: &operatorv1.Klusterlet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+		},
+	}
+
+	if name == "" {
+		glog.V(100).Info("The name of the Klusterlet is empty")
+
+		return nil, fmt.Errorf("klusterlet 'name' cannot be empty")
+	}
+
+	if !builder.Exists() {
+		glog.V(100).Infof("The Klusterlet %s does not exist", name)
+
+		return nil, fmt.Errorf("klusterlet object %s does not exist", name)
+	}
+
+	builder.Definition = builder.Object
+
+	return builder, nil
+}
+
+// Get returns the Klusterlet object if found.
+func (builder *KlusterletBuilder) Get() (*operatorv1.Klusterlet, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Getting Klusterlet object %s", builder.Definition.Name)
+
+	klusterlet := &operatorv1.Klusterlet{}
+	err := builder.apiClient.Get(context.TODO(), runtimeclient.ObjectKey{Name: builder.Definition.Name}, klusterlet)
+
+	if err != nil {
+		glog.V(100).Infof("Failed to get Klusterlet object %s: %v", builder.Definition.Name, err)
+
+		return nil, err
+	}
+
+	return klusterlet, nil
+}
+
+// Exists checks whether this Klusterlet exists on the cluster.
+func (builder *KlusterletBuilder) Exists() bool {
+	if valid, _ := builder.validate(); !valid {
+		return false
+	}
+
+	glog.V(100).Infof("Checking if Klusterlet %s", builder.Definition.Name)
+
+	var err error
+	builder.Object, err = builder.Get()
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+// Create makes a Klusterlet on the cluster if it does not already exist.
+func (builder *KlusterletBuilder) Create() (*KlusterletBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Creating Klusterlet %s", builder.Definition.Name)
+
+	if builder.Exists() {
+		return builder, nil
+	}
+
+	err := builder.apiClient.Create(context.TODO(), builder.Definition)
+	if err != nil {
+		return nil, err
+	}
+
+	builder.Object = builder.Definition
+
+	return builder, nil
+}
+
+// Update changes the existing Klusterlet resource on the cluster to match the builder Definition. Deleting a Klusterlet
+// is nontrivial and affects the connection to the hub cluster, so there is no force option to allow deleting and
+// recreating the Klusterlet if the update fails.
+func (builder *KlusterletBuilder) Update() (*KlusterletBuilder, error) {
+	if valid, err := builder.validate(); !valid {
+		return nil, err
+	}
+
+	glog.V(100).Infof("Updating Klusterlet %s", builder.Definition.Name)
+
+	if !builder.Exists() {
+		glog.V(100).Infof("Klusterlet %s does not exist", builder.Definition.Name)
+
+		return nil, fmt.Errorf("cannot update non-existent klusterlet")
+	}
+
+	builder.Definition.ResourceVersion = builder.Object.ResourceVersion
+
+	err := builder.apiClient.Update(context.TODO(), builder.Definition)
+	if err != nil {
+		return nil, err
+	}
+
+	builder.Object = builder.Definition
+
+	return builder, nil
+}
+
+// Delete removes a Klusterlet from the cluster if it exists.
+func (builder *KlusterletBuilder) Delete() error {
+	if valid, err := builder.validate(); !valid {
+		return err
+	}
+
+	glog.V(100).Infof("Deleting Klusterlet %s", builder.Definition.Name)
+
+	if !builder.Exists() {
+		glog.V(100).Infof("Klusterlet %s does not exist", builder.Definition.Name)
+
+		builder.Object = nil
+
+		return nil
+	}
+
+	err := builder.apiClient.Delete(context.TODO(), builder.Object)
+	if err != nil {
+		return err
+	}
+
+	builder.Object = nil
+
+	return nil
+}
+
+// validate checks that the builder, definition, and apiClient are properly initialized and there is no errorMsg.
+func (builder *KlusterletBuilder) validate() (bool, error) {
+	resourceCRD := "klusterlet"
+
+	if builder == nil {
+		glog.V(100).Infof("The %s builder is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf("error: received nil %s builder", resourceCRD)
+	}
+
+	if builder.Definition == nil {
+		glog.V(100).Infof("The %s is uninitialized", resourceCRD)
+
+		return false, fmt.Errorf(msg.UndefinedCrdObjectErrString(resourceCRD))
+	}
+
+	if builder.apiClient == nil {
+		glog.V(100).Infof("The %s builder apiClient is nil", resourceCRD)
+
+		return false, fmt.Errorf("%s builder cannot have nil apiClient", resourceCRD)
+	}
+
+	if builder.errorMsg != "" {
+		glog.V(100).Infof("The %s builder has error message %s", resourceCRD, builder.errorMsg)
+
+		return false, fmt.Errorf(builder.errorMsg)
+	}
+
+	return true, nil
+}
---
+++ pkg/ocm/klusterlet_test.go
@@ -0,0 +1,366 @@
+package ocm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	operatorv1 "open-cluster-management.io/api/operator/v1"
+)
+
+var operatorTestSchemes = []clients.SchemeAttacher{
+	operatorv1.Install,
+}
+
+func TestNewKlusterletBuilder(t *testing.T) {
+	testCases := []struct {
+		name          string
+		client        bool
+		expectedError string
+	}{
+		{
+			name:          KlusterletName,
+			client:        true,
+			expectedError: "",
+		},
+		{
+			name:          "",
+			client:        true,
+			expectedError: "klusterlet 'name' cannot be empty",
+		},
+		{
+			name:          KlusterletName,
+			client:        false,
+			expectedError: "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		var testSettings *clients.Settings
+
+		if testCase.client {
+			testSettings = clients.GetTestClients(clients.TestClientParams{})
+		}
+
+		testBuilder := NewKlusterletBuilder(testSettings, testCase.name)
+
+		if testCase.client {
+			assert.Equal(t, testCase.expectedError, testBuilder.errorMsg)
+
+			if testCase.expectedError == "" {
+				assert.Equal(t, testCase.name, testBuilder.Definition.Name)
+			}
+		} else {
+			assert.Nil(t, testBuilder)
+		}
+	}
+}
+
+func TestPullKlusterlet(t *testing.T) {
+	testCases := []struct {
+		name                string
+		addToRuntimeObjects bool
+		client              bool
+		expectedError       error
+	}{
+		{
+			name:                KlusterletName,
+			addToRuntimeObjects: true,
+			client:              true,
+			expectedError:       nil,
+		},
+		{
+			name:                "",
+			addToRuntimeObjects: true,
+			client:              true,
+			expectedError:       fmt.Errorf("klusterlet 'name' cannot be empty"),
+		},
+		{
+			name:                KlusterletName,
+			addToRuntimeObjects: false,
+			client:              true,
+			expectedError:       fmt.Errorf("klusterlet object %s does not exist", KlusterletName),
+		},
+		{
+			name:                KlusterletName,
+			addToRuntimeObjects: true,
+			client:              false,
+			expectedError:       fmt.Errorf("klusterlet 'apiClient' cannot be nil"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		var (
+			runtimeObjects []runtime.Object
+			testSettings   *clients.Settings
+		)
+
+		if testCase.addToRuntimeObjects {
+			runtimeObjects = append(runtimeObjects, buildDummyKlusterlet(KlusterletName))
+		}
+
+		if testCase.client {
+			testSettings = clients.GetTestClients(clients.TestClientParams{
+				K8sMockObjects:  runtimeObjects,
+				SchemeAttachers: operatorTestSchemes,
+			})
+		}
+
+		testBuilder, err := PullKlusterlet(testSettings, testCase.name)
+		assert.Equal(t, testCase.expectedError, err)
+
+		if testCase.expectedError == nil {
+			assert.Equal(t, testCase.name, testBuilder.Definition.Name)
+		}
+	}
+}
+
+func TestKlusterletGet(t *testing.T) {
+	testCases := []struct {
+		testBuilder   *KlusterletBuilder
+		expectedError string
+	}{
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: "",
+		},
+		{
+			testBuilder:   buildInvalidKlusterlettestbuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: "klusterlet 'name' cannot be empty",
+		},
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: fmt.Sprintf("klusterlets.operator.open-cluster-management.io \"%s\" not found", KlusterletName),
+		},
+	}
+
+	for _, testCase := range testCases {
+		klusterlet, err := testCase.testBuilder.Get()
+
+		if testCase.expectedError == "" {
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.testBuilder.Definition.Name, klusterlet.Name)
+		} else {
+			assert.EqualError(t, err, testCase.expectedError)
+		}
+	}
+}
+
+func TestKlusterletExists(t *testing.T) {
+	testCases := []struct {
+		testBuilder *KlusterletBuilder
+		exists      bool
+	}{
+		{
+			testBuilder: buildValidKlusterletTestBuilder(buildTestClientWithDummyKlusterlet()),
+			exists:      true,
+		},
+		{
+			testBuilder: buildInvalidKlusterlettestbuilder(buildTestClientWithDummyKlusterlet()),
+			exists:      false,
+		},
+		{
+			testBuilder: buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			exists:      false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		exists := testCase.testBuilder.Exists()
+		assert.Equal(t, testCase.exists, exists)
+	}
+}
+
+func TestKlusterletCreate(t *testing.T) {
+	testCases := []struct {
+		testBuilder   *KlusterletBuilder
+		expectedError error
+	}{
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: nil,
+		},
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: nil,
+		},
+		{
+			testBuilder:   buildInvalidKlusterlettestbuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: fmt.Errorf("klusterlet 'name' cannot be empty"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		testBuilder, err := testCase.testBuilder.Create()
+		assert.Equal(t, testCase.expectedError, err)
+
+		if testCase.expectedError == nil {
+			assert.Equal(t, testBuilder.Definition.Name, testBuilder.Object.Name)
+		}
+	}
+}
+
+func TestKlusterletUpdate(t *testing.T) {
+	testCases := []struct {
+		testBuilder   *KlusterletBuilder
+		expectedError error
+	}{
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: nil,
+		},
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: fmt.Errorf("cannot update non-existent klusterlet"),
+		},
+		{
+			testBuilder:   buildInvalidKlusterlettestbuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: fmt.Errorf("klusterlet 'name' cannot be empty"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		assert.Empty(t, testCase.testBuilder.Definition.Spec.ClusterName)
+
+		testCase.testBuilder.Definition.Spec.ClusterName = "test"
+		testCase.testBuilder.Definition.ResourceVersion = "999"
+
+		testBuilder, err := testCase.testBuilder.Update()
+		assert.Equal(t, testCase.expectedError, err)
+
+		if testCase.expectedError == nil {
+			assert.Equal(t, "test", testBuilder.Object.Spec.ClusterName)
+		}
+	}
+}
+
+func TestKlusterletDelete(t *testing.T) {
+	testCases := []struct {
+		testBuilder   *KlusterletBuilder
+		expectedError error
+	}{
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: nil,
+		},
+		{
+			testBuilder:   buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{})),
+			expectedError: nil,
+		},
+		{
+			testBuilder:   buildInvalidKlusterlettestbuilder(buildTestClientWithDummyKlusterlet()),
+			expectedError: fmt.Errorf("klusterlet 'name' cannot be empty"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := testCase.testBuilder.Delete()
+		assert.Equal(t, testCase.expectedError, err)
+
+		if testCase.expectedError == nil {
+			assert.Nil(t, testCase.testBuilder.Object)
+		}
+	}
+}
+
+func TestKlusterletValidate(t *testing.T) {
+	testCases := []struct {
+		builderNil      bool
+		definitionNil   bool
+		apiClientNil    bool
+		builderErrorMsg string
+		expectedError   error
+	}{
+		{
+			builderNil:      false,
+			definitionNil:   false,
+			apiClientNil:    false,
+			builderErrorMsg: "",
+			expectedError:   nil,
+		},
+		{
+			builderNil:      true,
+			definitionNil:   false,
+			apiClientNil:    false,
+			builderErrorMsg: "",
+			expectedError:   fmt.Errorf("error: received nil klusterlet builder"),
+		},
+		{
+			builderNil:      false,
+			definitionNil:   true,
+			apiClientNil:    false,
+			builderErrorMsg: "",
+			expectedError:   fmt.Errorf("can not redefine the undefined klusterlet"),
+		},
+		{
+			builderNil:      false,
+			definitionNil:   false,
+			apiClientNil:    true,
+			builderErrorMsg: "",
+			expectedError:   fmt.Errorf("klusterlet builder cannot have nil apiClient"),
+		},
+		{
+			builderNil:      false,
+			definitionNil:   false,
+			apiClientNil:    false,
+			builderErrorMsg: "test error",
+			expectedError:   fmt.Errorf("test error"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		testBuilder := buildValidKlusterletTestBuilder(clients.GetTestClients(clients.TestClientParams{}))
+
+		if testCase.builderNil {
+			testBuilder = nil
+		}
+
+		if testCase.definitionNil {
+			testBuilder.Definition = nil
+		}
+
+		if testCase.apiClientNil {
+			testBuilder.apiClient = nil
+		}
+
+		if testCase.builderErrorMsg != "" {
+			testBuilder.errorMsg = testCase.builderErrorMsg
+		}
+
+		valid, err := testBuilder.validate()
+		assert.Equal(t, testCase.expectedError, err)
+		assert.Equal(t, testCase.expectedError == nil, valid)
+	}
+}
+
+// buildDummyKlusterlet returns a Klusterlet with the provided name.
+func buildDummyKlusterlet(name string) *operatorv1.Klusterlet {
+	return &operatorv1.Klusterlet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+}
+
+// buildTestClientWithDummyKlusterlet returns a test client with a Klusterlet using KlusterletName.
+func buildTestClientWithDummyKlusterlet() *clients.Settings {
+	return clients.GetTestClients(clients.TestClientParams{
+		K8sMockObjects: []runtime.Object{
+			buildDummyKlusterlet(KlusterletName),
+		},
+		SchemeAttachers: operatorTestSchemes,
+	})
+}
+
+// buildValidKlusterletTestBuilder returns a valid KlusterletBuilder using KlusterletName.
+func buildValidKlusterletTestBuilder(apiClient *clients.Settings) *KlusterletBuilder {
+	return NewKlusterletBuilder(apiClient, KlusterletName)
+}
+
+// buildInvalidKlusterlettestbuilder returns an invalid KlusterletBuilder with an empty name.
+func buildInvalidKlusterlettestbuilder(apiClient *clients.Settings) *KlusterletBuilder {
+	return NewKlusterletBuilder(apiClient, "")
+}
