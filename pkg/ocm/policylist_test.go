cec1d5554c72c0fa78761abc11f289922c2d11a3
Kirsten Laskoski <klaskosk@redhat.com>
2025-02-12 17:04:41 -0500

    ocm: add WaitForAllPoliciesComplianceState function (#906)
    Although there is already a way to wait for an individual policy to reach a certain compliance state, there is no way to wait for all policies to reach a compliance state. This PR addresses this gap by adding the WaitForAllPoliciesComplianceState function.
    
    In the context of a spoke cluster, this function can be used directly to wait for policies to all go compliant. For a hub cluster, one may provide ListOptions to specify a label or namespace to wait on the policies of a given ManagedCluster.

--- pkg/ocm/policylist_test.go
+++ pkg/ocm/policylist_test.go
@@ -1,12 +1,16 @@
 package ocm
 
 import (
+	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/openshift-kni/eco-goinfra/pkg/clients"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -67,5 +71,62 @@ func TestListPoliciesInAllNamespaces(t *testing.T) {
 		if testCase.expectedError == nil && len(testCase.listOptions) == 0 {
 			assert.Equal(t, len(testCase.policies), len(builders))
 		}
+	}
+}
+
+func TestWaitForAllPoliciesComplianceState(t *testing.T) {
+	testCases := []struct {
+		compliant     bool
+		client        bool
+		listOptions   []runtimeclient.ListOptions
+		expectedError error
+	}{
+		{
+			compliant:     true,
+			client:        true,
+			listOptions:   nil,
+			expectedError: nil,
+		},
+		{
+			compliant:     false,
+			client:        true,
+			listOptions:   nil,
+			expectedError: context.DeadlineExceeded,
+		},
+		{
+			compliant:     true,
+			client:        false,
+			listOptions:   nil,
+			expectedError: fmt.Errorf("failed to wait for policies compliance state, 'apiClient' parameter is nil"),
+		},
+		{
+			compliant: true,
+			client:    true,
+			listOptions: []runtimeclient.ListOptions{
+				{LabelSelector: labels.NewSelector()},
+				{LabelSelector: labels.NewSelector()},
+			},
+			expectedError: fmt.Errorf("error: more than one ListOptions was passed"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		var testSettings *clients.Settings
+
+		if testCase.client {
+			policy := buildDummyPolicy(defaultPolicyName, defaultPolicyNsName)
+
+			if testCase.compliant {
+				policy.Status.ComplianceState = policiesv1.Compliant
+			}
+
+			testSettings = clients.GetTestClients(clients.TestClientParams{
+				K8sMockObjects:  []runtime.Object{policy},
+				SchemeAttachers: policyTestSchemes,
+			})
+		}
+
+		err := WaitForAllPoliciesComplianceState(testSettings, policiesv1.Compliant, time.Second, testCase.listOptions...)
+		assert.Equal(t, testCase.expectedError, err)
 	}
 }
