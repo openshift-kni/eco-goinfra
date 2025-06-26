4efbb2bd4d45c9b53266c963a44a06a6123b86a2
kononovn <10511306+kononovn@users.noreply.github.com>
2025-06-26 00:54:50 +0000

    sync: update external libs

--- pkg/schemes/kmm/v1beta1/module_types.go
+++ pkg/schemes/kmm/v1beta1/module_types.go
@@ -303,7 +303,7 @@ type DevicePluginSpec struct {
 	Container DevicePluginContainerSpec `json:"container"`
 
 	// +optional
-	// InitContainer defines the init container that will be used by the device plugin
+	// InitContainer allows defines the init container that will be used by the device plugin
 	InitContainer *DevicePluginContainerSpec `json:"initContainer,omitempty"`
 
 	// +optional
