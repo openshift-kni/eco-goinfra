f7904e8ee554b7d42b86c700c723baa126690689
Kirsten Laskoski <klaskosk@redhat.com>
2025-08-17 09:15:52 -0400

    global: update to Go 1.25.0
    Related-to: openshift-kni/eco-gotests#746
    
    This PR updates Go to 1.25.0 and takes a new approach of separating the go and toolchain directives. This allows us to express compatibility with all go1.25 versions while specifying a more exact one to build with.
    
    Similarly, golangci-lint has been updated to 2.4.0 for Go 1.25 support.

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -4,7 +4,7 @@ set -e
 
 . "$(dirname "$0")"/common.sh
 
-GOLANGCI_LINT_VERSION="2.2.2"
+GOLANGCI_LINT_VERSION="2.4.0"
 
 # IsGoLangCiLintInstalled is used to check whether golangci-lint executable is on the $PATH.
 function IsGolangCiLintInstalled() {
